3453d81eca87a75ca307e22dc98f13dc14e11a2e
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-07-25 03:04:20 -0400

    Update MainActivity.java

--- app/src/main/java/com/campos/baseconverter/MainActivity.java
+++ app/src/main/java/com/campos/baseconverter/MainActivity.java
@@ -1,8 +1,13 @@
 package com.campos.baseconverter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mi_about:
+                aboutClicked();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void aboutClicked() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
