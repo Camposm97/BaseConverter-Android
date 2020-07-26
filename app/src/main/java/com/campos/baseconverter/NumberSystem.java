a2a3d20bdf1620796a6928dbb7b9eec47c3ed2cd
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-07-26 10:48:09 -0400

    Update NumberSystem.java

--- app/src/main/java/com/campos/baseconverter/NumberSystem.java
+++ app/src/main/java/com/campos/baseconverter/NumberSystem.java
@@ -1,8 +1,32 @@
 package com.campos.baseconverter;
 
 public class NumberSystem {
+    private Base convertFrom;
+    private Base convertTo;
 
     public NumberSystem() {
+        this.convertFrom = null;
+        this.convertTo = null;
+    }
 
+    public Base getConvertFrom() {
+        return convertFrom;
+    }
+
+    public void setConvertFrom(Base convertFrom) {
+        this.convertFrom = convertFrom;
+    }
+
+    public Base getConvertTo() {
+        return convertTo;
+    }
+
+    public void setConvertTo(Base convertTo) {
+        this.convertTo = convertTo;
+    }
+
+    public String convert(String number) {
+
+        return null;
     }
 }
