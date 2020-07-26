f0849894fc89fbb620466043f69600c40a6c77aa
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-07-26 17:41:03 -0400

    Create BaseComparator.java

---
+++ app/src/main/java/com/campos/baseconverter/BaseComparator.java
@@ -0,0 +1,11 @@
+package com.campos.baseconverter;
+
+import java.util.Comparator;
+
+public class BaseComparator implements Comparator<Base> {
+    @Override
+    public int compare(Base b1, Base b2) {
+
+        return 0;
+    }
+}
