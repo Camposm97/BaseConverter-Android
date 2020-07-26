57d914e3f0b71c38d425c87b1c9229998a234074
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-07-26 18:18:33 -0400

    Create BaseComparatorTest.java
    Implement tests for BaseComparator

---
+++ app/src/test/java/com/campos/baseconverter/BaseComparatorTest.java
@@ -0,0 +1,11 @@
+package com.campos.baseconverter;
+
+import org.junit.Test;
+
+public class BaseComparatorTest {
+
+    @Test
+    public void testBaseComparatorEqualsOne() {
+        System.out.println("Print one");
+    }
+}
