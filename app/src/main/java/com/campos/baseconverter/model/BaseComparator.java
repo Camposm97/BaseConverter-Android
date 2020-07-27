0239fab4126188ecd3c1153cd370dd1542b5d3b2
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-07-26 20:30:24 -0400

    Update BaseComparator.java

--- app/src/main/java/com/campos/baseconverter/model/BaseComparator.java
+++ app/src/main/java/com/campos/baseconverter/model/BaseComparator.java
@@ -9,9 +9,9 @@ public class BaseComparator implements Comparator<Base> {
         int radix2 = b2.getRadix();
 
         if (radix1 < radix2) {
-            return 1; // Then we multiply
+            return 1; // 1 = We're converting to a higher base
         } else if (radix1 > radix2) {
-            return 2; // Then we divide
+            return -1; // -1 = We're converting to a lower base
         }
         return 0;
     }
