c6d5b0035955a7a95456ffaac5dcdd9cc209319a
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-07-29 01:41:51 -0400

    Update BaseConverter.java

--- app/src/main/java/com/campos/baseconverter/model/BaseConverter.java
+++ app/src/main/java/com/campos/baseconverter/model/BaseConverter.java
@@ -28,19 +28,6 @@ public class BaseConverter {
     /**
      * The way convert() works is that the user inputs their number as a String so the method can take the input and
      * analyze it and see if converting from its base to the base wanted is possible.
-     *
-     * If the bases we're converting from and to are the same, then return the same thing
-     *
-     * If the base we're converting to is lower from decimal, then all we have to do is divide.
-     *
-     * If the base we're converting to is decimal and the base we're converting from is smaller than decimal,
-     * then all we have to is multiply by the base we're converting from
-     *
-     * If the base we're converting to is higher from a smaller base,
-     * then we have to take the input and convert to decimal and then convert that to the base we want by dividing
-     *
-     * If the base we're converting to is lower than the base we're converting from, then we have to take the input and convert it to deciaml and then,
-     * convert it to the base we want by dividing
      * @param str
      * @return result
      */
