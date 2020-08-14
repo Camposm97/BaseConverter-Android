6a337909342c0be8aaf89b85cb47ba0ff980741a
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-14 19:26:29 -0400

    Created fragments package

--- app/src/main/java/com/campos/baseconverter/fragments/AllBasesFragment.java
+++ app/src/main/java/com/campos/baseconverter/fragments/AllBasesFragment.java
@@ -1,4 +1,4 @@
-package com.campos.baseconverter;
+package com.campos.baseconverter.fragments;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.campos.baseconverter.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MainBasesFragment#newInstance} factory method to
+ * Use the {@link AllBasesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MainBasesFragment extends Fragment {
+public class AllBasesFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class MainBasesFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public MainBasesFragment() {
+    public AllBasesFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class MainBasesFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment MainBasesFragment.
+     * @return A new instance of fragment AllBasesFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MainBasesFragment newInstance(String param1, String param2) {
-        MainBasesFragment fragment = new MainBasesFragment();
+    public static AllBasesFragment newInstance(String param1, String param2) {
+        AllBasesFragment fragment = new AllBasesFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class MainBasesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_bases, container, false);
+        return inflater.inflate(R.layout.fragment_all_bases, container, false);
     }
 }
--- app/src/main/java/com/campos/baseconverter/fragments/MainBasesFragment.java
+++ app/src/main/java/com/campos/baseconverter/fragments/MainBasesFragment.java
@@ -1,4 +1,4 @@
-package com.campos.baseconverter;
+package com.campos.baseconverter.fragments;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.campos.baseconverter.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AllBasesFragment#newInstance} factory method to
+ * Use the {@link MainBasesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AllBasesFragment extends Fragment {
+public class MainBasesFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class AllBasesFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public AllBasesFragment() {
+    public MainBasesFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class AllBasesFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment AllBasesFragment.
+     * @return A new instance of fragment MainBasesFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AllBasesFragment newInstance(String param1, String param2) {
-        AllBasesFragment fragment = new AllBasesFragment();
+    public static MainBasesFragment newInstance(String param1, String param2) {
+        MainBasesFragment fragment = new MainBasesFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class AllBasesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_bases, container, false);
+        return inflater.inflate(R.layout.fragment_main_bases, container, false);
     }
 }
