6652842ecdd4e9cb597c00f7b0c0120224ce6a4a
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-14 19:29:30 -0400

    Create MyFragmentStatePageAdapter.java

---
+++ app/src/main/java/com/campos/baseconverter/model/MyFragmentStatePageAdapter.java
@@ -0,0 +1,19 @@
+package com.campos.baseconverter.model;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class MyFragmentStatePageAdapter extends FragmentStatePagerAdapter {
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
