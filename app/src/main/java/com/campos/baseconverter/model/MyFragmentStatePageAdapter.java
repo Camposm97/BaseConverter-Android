7a9444fdf31416287032d95310b30aea9ff8806c
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-15 00:04:40 -0400

    Update MyFragmentStatePageAdapter.java
    Superclass changed to FragmentStateAdapter

--- app/src/main/java/com/campos/baseconverter/model/MyFragmentStatePageAdapter.java
+++ app/src/main/java/com/campos/baseconverter/model/MyFragmentStatePageAdapter.java
@@ -1,19 +1,37 @@
 package com.campos.baseconverter.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-public class MyFragmentStatePageAdapter extends FragmentStatePagerAdapter {
+public class MyFragmentStatePageAdapter extends FragmentStateAdapter {
+    private final List<Fragment> fragmentList = new LinkedList<>();
+    private final List<String> titleList = new LinkedList<>();
+
+    public MyFragmentStatePageAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+
+    private void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+        titleList.add(title);
+    }
 
     @NonNull
     @Override
-    public Fragment getItem(int position) {
-        return null;
+    public Fragment createFragment(int position) {
+        return fragmentList.get(position);
     }
 
     @Override
-    public int getCount() {
-        return 0;
+    public int getItemCount() {
+        return fragmentList.size();
     }
 }
