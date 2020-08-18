27ebb7817e6144b190fa83773a30fb6232ba3128
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-17 23:23:08 -0400

    Update MainBasesFragment.java
    Deleting onCreate()

--- app/src/main/java/com/campos/baseconverter/fragments/MainBasesFragment.java
+++ app/src/main/java/com/campos/baseconverter/fragments/MainBasesFragment.java
@@ -14,37 +14,13 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import com.campos.baseconverter.R;
+import com.campos.baseconverter.model.BaseTextChangeListener;
 
 public class MainBasesFragment extends Fragment {
-    public MainBasesFragment() {
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EditText editText1 = getActivity().findViewById(R.id.tf_bin);
-        editText1.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        
         return inflater.inflate(R.layout.fragment_main_bases, container, false);
     }
 }
