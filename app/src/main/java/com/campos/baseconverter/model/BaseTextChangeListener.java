72b8891eea759e5edf6d7c9f85031c5a160e1aed
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-17 23:16:00 -0400

    Update BaseTextChangeListener.java
    Implementing methods from TextWatcher interface

--- app/src/main/java/com/campos/baseconverter/model/BaseTextChangeListener.java
+++ app/src/main/java/com/campos/baseconverter/model/BaseTextChangeListener.java
@@ -2,20 +2,24 @@ package com.campos.baseconverter.model;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 
 public class BaseTextChangeListener implements TextWatcher {
+    private static final String TAG = BaseTextChangeListener.class.getSimpleName();
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+        Log.d(TAG, "Before: " + s);
     }
 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
+        Log.d(TAG, "onChange: " + s);
 
     }
 
     @Override
     public void afterTextChanged(Editable s) {
+        Log.d(TAG, "After: " + s);
 
     }
 }
