4c750570b97dfe5e1ebadc2b3f753051294ea162
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-18 19:48:30 -0400

    Update BaseTextChangeListener.java
    Now an abstract class that implements beforeTextChanged() and afterTextChanged() doing nothing.

--- app/src/main/java/com/campos/baseconverter/model/BaseTextChangeListener.java
+++ app/src/main/java/com/campos/baseconverter/model/BaseTextChangeListener.java
@@ -4,32 +4,10 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 
-public interface BaseTextChangeListener extends TextWatcher {
-
-    public BaseTextChangeListener(EditText tf) {
-        this.tf = tf;
-        this.strBefore = "";
-    }
-
+public abstract class BaseTextChangeListener implements TextWatcher {
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        this.strBefore = s.toString();
-    }
 
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-        String str = s.toString();
-        if (!str.isEmpty()) {
-            for (char c : str.toCharArray()) {
-                try {
-                    if (c != '0' || c != '1') {
-                        tf.setText(strBefore);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 
     @Override
