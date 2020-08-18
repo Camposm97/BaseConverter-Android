7c5f1f7fb06de4ed3c541b4b9af5a9773b9d8028
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-17 23:11:31 -0400

    Update MainBasesFragment.java

--- app/src/main/java/com/campos/baseconverter/fragments/MainBasesFragment.java
+++ app/src/main/java/com/campos/baseconverter/fragments/MainBasesFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -21,7 +23,22 @@ public class MainBasesFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EditText editText1 = getActivity().findViewById(R.id.tf_bin);
-        
+        editText1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     @Override
