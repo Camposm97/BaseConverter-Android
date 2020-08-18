bd50b33fa201510a5036fc44c55b29a7ece5d975
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-17 23:07:56 -0400

    Update AllBasesFragment.java

--- app/src/main/java/com/campos/baseconverter/fragments/AllBasesFragment.java
+++ app/src/main/java/com/campos/baseconverter/fragments/AllBasesFragment.java
@@ -17,10 +17,6 @@ public class AllBasesFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
