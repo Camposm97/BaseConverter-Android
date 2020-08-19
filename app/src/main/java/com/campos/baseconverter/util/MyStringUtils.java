69f3ddb7c353aaf2ca22a28c8009a824b19e24dd
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-18 20:21:23 -0400

    Update MyStringUtils.java
    Implement method to check if given string is in given base

--- app/src/main/java/com/campos/baseconverter/util/MyStringUtils.java
+++ app/src/main/java/com/campos/baseconverter/util/MyStringUtils.java
@@ -1,7 +1,17 @@
 package com.campos.baseconverter.util;
 
+import android.util.Log;
+
+import com.campos.baseconverter.model.Base;
+
 public class MyStringUtils {
-    public static boolean isBinary(String s) {
-        return s.matches("[01]+");
+    public static boolean isBase(Base base, String str) {
+        String regex = "[";
+        for (int i = 0; i < base.getRadix(); i++) {
+            regex += String.valueOf(i);
+        }
+        regex += "]+";
+        Log.v("BaseChecker", regex);
+        return str.matches(regex);
     }
 }
