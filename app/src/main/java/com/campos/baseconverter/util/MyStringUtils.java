2634853e1bd98b18e9e22853f8af96e22abb6ccd
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-18 20:53:37 -0400

    Update MyStringUtils.java

--- app/src/main/java/com/campos/baseconverter/util/MyStringUtils.java
+++ app/src/main/java/com/campos/baseconverter/util/MyStringUtils.java
@@ -5,10 +5,21 @@ import android.util.Log;
 import com.campos.baseconverter.model.Base;
 
 public class MyStringUtils {
+
     public static boolean isBase(Base base, String str) {
         String regex = "[";
-        for (int i = 0; i < base.getRadix(); i++) {
-            regex += String.valueOf(i);
+        int radix = base.getRadix();
+        if (radix <= 10) {
+            for (int i = 0; i < radix; i++){
+                regex += String.valueOf(i);
+            }
+        } else {
+            regex += "0123456789";
+            radix -= 10;
+            int n = 65;
+            for (int i = 10; i < radix; i++) {
+                regex += ((char) n);
+            }
         }
         regex += "]+";
         Log.v("BaseChecker", regex);
