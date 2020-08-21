e8339adb82a44e6648dfd28e9a1ba3ed2c72cee8
Camposm <camposm1205@gmail.com>
2020-08-21 19:48:41 -0400

    Update BaseOnKeyListener.java
    Fixed logic error

--- app/src/main/java/com/campos/baseconverter/model/BaseOnKeyListener.java
+++ app/src/main/java/com/campos/baseconverter/model/BaseOnKeyListener.java
@@ -1,25 +1,35 @@
 package com.campos.baseconverter.model;
 
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 
 import com.campos.baseconverter.util.MyUtils;
 
+import java.util.Arrays;
+
 public class BaseOnKeyListener implements View.OnKeyListener {
-    private BaseConverter bc;
+    private static final String TAG = BaseOnKeyListener.class.getSimpleName();
     private EditText tf;
     private EditText[] arr;
+    private Base convertFrom;
 
-    public BaseOnKeyListener(EditText tf, Base convertFrom, EditText[] arr) {
-        this.bc = new BaseConverter(tf.getText().toString(), convertFrom);
+
+    public BaseOnKeyListener(EditText tf, EditText[] arr, Base convertFrom) {
         this.tf = tf;
         this.arr = arr;
+        this.convertFrom = convertFrom;
     }
 
     @Override
     public boolean onKey(View v, int keyCode, KeyEvent event) {
         if (isInfoValid()) {
+            BaseConverter bc = new BaseConverter();
+            bc.setInput(tf.getText().toString());
+            bc.setConvertFrom(convertFrom);
+            String[] results = bc.getMainResults();
+            Log.v(TAG, Arrays.toString(results));
             displayResults(bc.getMainResults());
         }
         return false;
@@ -28,12 +38,12 @@ public class BaseOnKeyListener implements View.OnKeyListener {
     private void displayResults(String[] results) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != tf) {
-            arr[i].setText(results[i]);
+                arr[i].setText(results[i]);
             }
         }
     }
 
     public boolean isInfoValid() {
-        return MyUtils.isValidBase(bc.getConvertFrom(), tf.getText().toString());
+        return MyUtils.isValidBase(convertFrom, tf.getText().toString());
     }
 }
