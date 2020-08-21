95abfebce75f7ca23cff140192281d435514c021
Camposm <camposm1205@gmail.com>
2020-08-21 19:07:31 -0400

    Update BaseOnKeyListener.java

--- app/src/main/java/com/campos/baseconverter/model/BaseOnKeyListener.java
+++ app/src/main/java/com/campos/baseconverter/model/BaseOnKeyListener.java
@@ -1,39 +1,35 @@
 package com.campos.baseconverter.model;
 
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 
 import com.campos.baseconverter.util.MyUtils;
 
-import java.util.Arrays;
-
 public class BaseOnKeyListener implements View.OnKeyListener {
     private BaseConverter bc;
     private EditText tf;
-    private EditText[] editTexts;
+    private EditText[] arr;
 
-    public BaseOnKeyListener(EditText tf, Base convertFrom, EditText[] editTexts) {
+    public BaseOnKeyListener(EditText tf, Base convertFrom, EditText[] arr) {
         this.bc = new BaseConverter(tf.getText().toString(), convertFrom);
         this.tf = tf;
-        this.editTexts = editTexts;
+        this.arr = arr;
     }
 
     @Override
     public boolean onKey(View v, int keyCode, KeyEvent event) {
         if (isInfoValid()) {
-            String[] results = bc.getMainResults();
-            displayResults(results);
+            displayResults(bc.getMainResults());
         }
         return false;
     }
 
     private void displayResults(String[] results) {
-        for (int i = 0; i < editTexts.length; i++) {
-//            if (arr[i] != tf) {
-            editTexts[i].setText(results[i]);
-//            }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != tf) {
+            arr[i].setText(results[i]);
+            }
         }
     }
 
