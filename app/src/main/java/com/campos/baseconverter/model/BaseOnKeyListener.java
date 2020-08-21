767a856e56a532aa311cc1ddcf55dccbd13f8d18
Camposm <camposm1205@gmail.com>
2020-08-21 18:52:22 -0400

    Update BaseOnKeyListener.java

--- app/src/main/java/com/campos/baseconverter/model/BaseOnKeyListener.java
+++ app/src/main/java/com/campos/baseconverter/model/BaseOnKeyListener.java
@@ -1,4 +1,20 @@
 package com.campos.baseconverter.model;
 
-public class BaseOnKeyListener {
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+
+public class BaseOnKeyListener implements View.OnKeyListener {
+    private BaseConverter bc;
+    private EditText tf;
+
+    public BaseOnKeyListener(EditText tf) {
+        this.tf = tf;
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+        return false;
+    }
 }
