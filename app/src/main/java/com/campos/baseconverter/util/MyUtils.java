768d14c667d06ad9a901ed069aeb017a686ed85e
Camposm <camposm1205@gmail.com>
2020-08-23 09:19:14 -0400

    Update MyUtils.java
    Removed unused methods

--- app/src/main/java/com/campos/baseconverter/util/MyUtils.java
+++ app/src/main/java/com/campos/baseconverter/util/MyUtils.java
@@ -5,22 +5,6 @@ import android.util.Log;
 import com.campos.baseconverter.model.Base;
 
 public class MyUtils {
-    public static boolean isBinary(String str) {
-        return str.matches("[01]+");
-    }
-
-    public static boolean isOct(String str) {
-        return str.matches("[0-7]+");
-    }
-
-    public static boolean isDec(String str) {
-        return str.matches("[0-9]+");
-    }
-
-    public static boolean isHex(String str) {
-        return str.matches("[0-9A-F]+");
-    }
-
     public static boolean isValidBase(Base base, String str) {
         switch (base) {
             case BINARY:
