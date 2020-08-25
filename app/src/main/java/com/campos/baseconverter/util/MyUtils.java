c6b9dc256f2eae850ffc1350842107261e62f981
Camposm <camposm1205@gmail.com>
2020-08-24 20:38:04 -0400

    Updated MyUtils.java
    Implemented method for formatting binary string to 4 bits between spaces

--- app/src/main/java/com/campos/baseconverter/util/MyUtils.java
+++ app/src/main/java/com/campos/baseconverter/util/MyUtils.java
@@ -23,4 +23,22 @@ public class MyUtils {
                 }
         }
     }
+
+    public static String formatBinStr(String input) {
+        String result = "";
+        int count = 0;
+        for (int i = input.length() - 1; i >= 0; i--) {
+            char c = input.charAt(i);
+            result = c + result;
+            count++;
+            if (count >= 4) {
+                result = " " + result;
+                count = 0;
+            }
+        }
+        if (result.charAt(0) == ' ') {
+            result = result.substring(1);
+        }
+        return result;
+    }
 }
