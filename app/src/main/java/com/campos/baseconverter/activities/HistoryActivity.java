171d0ffe0a1fd761fc86e462d72125d9d053077d
Camposm <camposm1205@gmail.com>
2020-08-24 20:57:38 -0400

    Update HistoryActivity.java
    Wrote comment for what the HistoryActivity can do

--- app/src/main/java/com/campos/baseconverter/activities/HistoryActivity.java
+++ app/src/main/java/com/campos/baseconverter/activities/HistoryActivity.java
@@ -6,6 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.campos.baseconverter.R;
 
+/**
+ * Everytime a user chooses a base to convert from, the HistoryActivity will
+ * display what that user input and base he/she chose.  Display it as a list view or
+ * whatever android provides to display it vertically.  The user can then click on that
+ * base and input he/she chose and it will either display a dialog or a fragment to
+ * show the bases.
+ * Maybe have it where when he/she clicks on the base and input, it will ask to
+ * convert it to what which can make use of the BaseConverter class more.
+ */
 public class HistoryActivity extends AppCompatActivity {
 
     @Override
