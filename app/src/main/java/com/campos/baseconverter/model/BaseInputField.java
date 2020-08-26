d905b111b4e8e575ae5d71cae662196cf3619a22
Camposm <camposm1205@gmail.com>
2020-08-26 19:51:29 -0400

    Create BaseInputField.java

---
+++ app/src/main/java/com/campos/baseconverter/model/BaseInputField.java
@@ -0,0 +1,10 @@
+package com.campos.baseconverter.model;
+
+import android.content.Context;
+import android.widget.EditText;
+
+public class BaseInputField extends androidx.appcompat.widget.AppCompatEditText {
+    public BaseInputField(Context context) {
+        super(context);
+    }
+}
