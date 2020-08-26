45739787ba6be9daa7c6af7b4c2c2e562daff4d6
Camposm <camposm1205@gmail.com>
2020-08-26 19:51:50 -0400

    Update MainBasesFragment.java
    Simplified code (sort of)

--- app/src/main/java/com/campos/baseconverter/fragments/MainBasesFragment.java
+++ app/src/main/java/com/campos/baseconverter/fragments/MainBasesFragment.java
@@ -1,10 +1,7 @@
 package com.campos.baseconverter.fragments;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.InputFilter;
-import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,8 +16,11 @@ import androidx.fragment.app.Fragment;
 import com.campos.baseconverter.R;
 import com.campos.baseconverter.model.Base;
 import com.campos.baseconverter.model.BaseConverter;
+import com.campos.baseconverter.model.BaseInputDialogBuilder;
 import com.campos.baseconverter.model.InvalidBaseNumberException;
 import com.campos.baseconverter.util.MyUtils;
+
+import java.util.List;
 
 
 public class MainBasesFragment extends Fragment {
@@ -33,7 +33,7 @@ public class MainBasesFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_main_bases, container, false);
         loadControls();
-        fillSpinner();
+        loadSpinner();
         return view;
     }
 
@@ -45,73 +45,73 @@ public class MainBasesFragment extends Fragment {
         tfHex = view.findViewById(R.id.tf_hex);
     }
 
-    public void fillSpinner() { // Simplify code later
-        String[] arr = {"Convert From", "Binary", "Octal", "Decimal", "Hexadecimal"};
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, arr);
+    public void loadSpinner() {
+        List<String> list = Base.loadSpinnerItemMainBases();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, list);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, final int position, long id) {
                 if (position != 0) {
-                    final EditText editText = new EditText(getContext());
-                    editText.setInputType(InputType.TYPE_CLASS_TEXT);
-                    editText.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
-                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getContext());
-                    alertBuilder.setCancelable(false);
-                    alertBuilder.setTitle("Convert From: " + spinner.getItemAtPosition(position));
-                    alertBuilder.setMessage("Please Enter Input:");
-                    alertBuilder.setView(editText);
-                    alertBuilder.setPositiveButton("Convert", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            String item = (String) spinner.getItemAtPosition(position);
-                            Base convertFrom = Base.valueOf(item.toUpperCase());
-                            String input = editText.getText().toString();
-                            try {
-                                BaseConverter baseConverter = new BaseConverter(convertFrom, input);
-                                String[] results = baseConverter.getMainResults();
-                                EditText[] arr = loadFieldArr();
-                                for (int i = 0; i < results.length; i++) {
-                                    if (i == 0) {
-                                        arr[i].setText(MyUtils.formatBinStr(results[i]));
-                                    } else {
-                                        arr[i].setText(results[i]);
-                                    }
-                                }
-                            } catch (InvalidBaseNumberException e) {
-                                Toast.makeText(getContext(), R.string.invalid_base_num_message, Toast.LENGTH_SHORT).show();
-                            } finally {
-                                spinner.setSelection(0);
-                            }
-                        }
-                    });
-                    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            clearFields();
-                            spinner.setSelection(0);
-                        }
-                    });
-                    alertBuilder.show();
+                    showBaseInputDialog(position);
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                // Do nothing
             }
         });
     }
 
+    public void showBaseInputDialog(int position) {
+        String chosenItem = (String) spinner.getItemAtPosition(position);
+        final Base convertFrom = Base.valueOf(chosenItem.toUpperCase());
+        BaseInputDialogBuilder dialogBuilder = new BaseInputDialogBuilder(getContext(), chosenItem);
+        final EditText tfInput = dialogBuilder.getTfInput();
+        dialogBuilder.setPositiveButton("Convert", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                attemptBaseConversion(convertFrom, tfInput.getText().toString());
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                clearFields();
+                spinner.setSelection(0);
+            }
+        });
+        dialogBuilder.show();
+    }
+
+    public void attemptBaseConversion(Base convertFrom, String input) {
+        try {
+            BaseConverter baseConverter = new BaseConverter(convertFrom, input);
+            String[] results = baseConverter.getMainResults();
+            EditText[] arr = loadOutputs();
+            for (int i = 0; i < results.length; i++) {
+                if (i == 0) {
+                    arr[i].setText(MyUtils.formatBinStr(results[i]));
+                } else {
+                    arr[i].setText(results[i]);
+                }
+            }
+        } catch (InvalidBaseNumberException e) {
+            Toast.makeText(getContext(), R.string.invalid_base_num_message, Toast.LENGTH_SHORT).show();
+        } finally {
+            spinner.setSelection(0);
+        }
+    }
+
     public void clearFields() {
-        EditText[] arr = loadFieldArr();
+        EditText[] arr = loadOutputs();
         for (int i = 0; i < arr.length; i++) {
             arr[i].getText().clear();
         }
     }
 
-    private EditText[] loadFieldArr() {
+    private EditText[] loadOutputs() {
         return new EditText[]{tfBin, tfOct, tfDec, tfHex};
     }
 }
