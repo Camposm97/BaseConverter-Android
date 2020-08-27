9ae22fcd6d5540ec23292e20b10101e0adab7cd9
Camposm <camposm1205@gmail.com>
2020-08-26 20:18:39 -0400

    Update MyUtils.java
    Removed isValisBaseNum()

--- app/src/main/java/com/campos/baseconverter/util/MyUtils.java
+++ app/src/main/java/com/campos/baseconverter/util/MyUtils.java
@@ -8,27 +8,6 @@ import com.campos.baseconverter.model.Base;
  * revolve around Bases.
  */
 public class MyUtils {
-    public static boolean isValidBaseNum(Base base, String input) {
-        switch (base) {
-            case BINARY:
-                return input.matches("[01]+");
-            case OCTAL:
-                return input.matches("[0-7]+");
-            case DECIMAL:
-                return input.matches("[0-9]+");
-            case HEXADECIMAL:
-                return input.matches("[0-9A-F]+");
-            default:
-                int radix = base.getRadix();
-                if (radix < 10) {
-                    return input.matches("[0-" + (radix - 1) + "]+");
-                } else {
-                    char c = (char) (54 + radix);
-                    return input.matches("[0-9A-" + c + "]+");
-                }
-        }
-    }
-
     public static String formatBinStr(String input) {
         String result = "";
         int count = 0;
