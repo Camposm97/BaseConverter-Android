4ad3ce371e03caa8e0dd574a8cfbf114a4bcc374
Camposm <camposm1205@gmail.com>
2020-08-26 20:02:26 -0400

    Update BaseInputField.java
    Modified it so the user can only enter upper case letters

--- app/src/main/java/com/campos/baseconverter/model/BaseInputField.java
+++ app/src/main/java/com/campos/baseconverter/model/BaseInputField.java
@@ -1,10 +1,14 @@
 package com.campos.baseconverter.model;
 
 import android.content.Context;
+import android.text.InputFilter;
+import android.text.InputType;
 import android.widget.EditText;
 
 public class BaseInputField extends androidx.appcompat.widget.AppCompatEditText {
     public BaseInputField(Context context) {
         super(context);
+        super.setInputType(InputType.TYPE_CLASS_TEXT);
+        super.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
     }
 }
