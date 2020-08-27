dbbd6c76b76275b006bfc06a30219e955abd8fdb
Camposm <camposm1205@gmail.com>
2020-08-26 20:02:46 -0400

    Update AllBasesFragment.java
    Simplified code simialr to MainBasesFragment

--- app/src/main/java/com/campos/baseconverter/fragments/AllBasesFragment.java
+++ app/src/main/java/com/campos/baseconverter/fragments/AllBasesFragment.java
@@ -22,6 +22,7 @@ import androidx.fragment.app.Fragment;
 import com.campos.baseconverter.R;
 import com.campos.baseconverter.model.Base;
 import com.campos.baseconverter.model.BaseConverter;
+import com.campos.baseconverter.model.BaseInputDialogBuilder;
 import com.campos.baseconverter.model.InvalidBaseNumberException;
 import com.campos.baseconverter.util.MyUtils;
 
@@ -32,35 +33,21 @@ import java.util.List;
 public class AllBasesFragment extends Fragment {
     private static final String TAG = AllBasesFragment.class.getSimpleName();
     private View root;
-    private LayoutInflater inflater;
-    private List<EditText> tfList;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private Spinner spinner;
+    private List<EditText> outputList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         this.root = inflater.inflate(R.layout.fragment_all_bases, container, false);
-        this.inflater = inflater;
-        this.tfList = new LinkedList<>();
+        this.outputList = new LinkedList<>();
         loadSpinner();
-        loadViews();
+        loadViewsInRoot();
         return root;
     }
 
-    public void loadViews() {
-        LinearLayout layoutOutputField = root.findViewById(R.id.layout_output_field);
-        LinearLayout[] layouts = loadOutputFields();
-        for (int i = 0; i < layouts.length; i++) {
-            layoutOutputField.addView(layouts[i]);
-        }
-    }
-
     public void loadSpinner() {
-        final Spinner spinner = root.findViewById(R.id.spinner_all_bases);
+        spinner = root.findViewById(R.id.spinner_all_bases);
         List<String> list = Base.loadSpinnerItemsAllBases();
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, list);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -69,55 +56,62 @@ public class AllBasesFragment extends Fragment {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, final int position, long id) {
                 if (position != 0) {
-                    final EditText editText = new EditText(getContext());
-                    editText.setInputType(InputType.TYPE_CLASS_TEXT);
-                    editText.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
-                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getContext());
-                    alertBuilder.setCancelable(false);
-                    alertBuilder.setTitle("Convert From: " + spinner.getItemAtPosition(position));
-                    alertBuilder.setMessage("Please Enter Input:");
-                    alertBuilder.setView(editText);
-                    alertBuilder.setPositiveButton("Convert", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Base convertFrom = Base.values()[position - 1];
-                            String input = editText.getText().toString();
-
-                            try {
-                                BaseConverter baseConverter = new BaseConverter(convertFrom, input);
-                                String[] results = baseConverter.getAllResults();
-                                Log.v(TAG, Arrays.toString(results));
-                                for (int i = 0; i < tfList.size(); i++) {
-                                    if (i == 0) {
-                                        tfList.get(i).setText(MyUtils.formatBinStr(results[i]));
-                                    } else {
-                                        tfList.get(i).setText(results[i]);
-                                    }
-                                }
-                            } catch (InvalidBaseNumberException e) {
-                                Toast.makeText(getContext(), R.string.invalid_base_num_message, Toast.LENGTH_SHORT).show();
-                            } finally {
-                                spinner.setSelection(0);
-                            }
-
-                        }
-                    });
-                    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            clearFields();
-                            spinner.setSelection(0);
-                        }
-                    });
-                    alertBuilder.show();
+                    showBaseInputDialog(position);
                 }
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                // Do Nothing
             }
         });
+    }
+
+    public void showBaseInputDialog(final int position) {
+        String chosenItem = (String) spinner.getItemAtPosition(position);
+        BaseInputDialogBuilder dialogBuilder = new BaseInputDialogBuilder(getContext(), chosenItem);
+        final EditText tfInput = dialogBuilder.getTfInput();
+        dialogBuilder.setPositiveButton("Convert", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Base convertFrom = Base.values()[position - 1];
+                String input = tfInput.getText().toString();
+                attemptBaseConversion(convertFrom, input);
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                clearFields();
+                spinner.setSelection(0);
+            }
+        });
+        dialogBuilder.show();
+    }
+
+    public void attemptBaseConversion(Base convertFrom, String input) {
+        try {
+            BaseConverter baseConverter = new BaseConverter(convertFrom, input);
+            String[] results = baseConverter.getAllResults();
+            Log.v(TAG, Arrays.toString(results));
+            for (int i = 0; i < outputList.size(); i++) {
+                if (i == 0) {
+                    outputList.get(i).setText(MyUtils.formatBinStr(results[i]));
+                } else {
+                    outputList.get(i).setText(results[i]);
+                }
+            }
+        } catch (InvalidBaseNumberException e) {
+            Toast.makeText(getContext(), R.string.invalid_base_num_message, Toast.LENGTH_SHORT).show();
+        } finally {
+            spinner.setSelection(0);
+        }
+    }
+
+    public void loadViewsInRoot() {
+        LinearLayout layoutOutputField = root.findViewById(R.id.layout_output_field);
+        LinearLayout[] layouts = loadOutputFields();
+        for (int i = 0; i < layouts.length; i++) {
+            layoutOutputField.addView(layouts[i]);
+        }
     }
 
     public LinearLayout[] loadOutputFields() {
@@ -129,20 +123,20 @@ public class AllBasesFragment extends Fragment {
                     LinearLayout.LayoutParams.WRAP_CONTENT);
             layoutParams.setMargins(0, 0, 0, 10);
 
-            arr[i] = (LinearLayout) inflater.inflate(R.layout.base_output_field_layout, null);
+            arr[i] = (LinearLayout) root.inflate(getContext(), R.layout.base_output_field_layout, null);
             arr[i].setLayoutParams(layoutParams);
 
             TextView lbl = (TextView) arr[i].getChildAt(0);
             lbl.setText(Base.toTitle(base));
 
             EditText tf = (EditText) arr[i].getChildAt(1);
-            tfList.add(tf);
+            outputList.add(tf);
         }
         return arr;
     }
 
     public void clearFields() {
-        for (EditText tf : tfList) {
+        for (EditText tf : outputList) {
             tf.getText().clear();
         }
     }
