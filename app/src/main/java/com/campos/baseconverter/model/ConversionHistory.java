d18080efbf41f3266828567b7dafeb2ef3238fbf
Camposm <camposm1205@gmail.com>
2020-08-28 18:52:08 -0400

    Update ConversionHistory.java
    Writing class method to save history

--- app/src/main/java/com/campos/baseconverter/model/ConversionHistory.java
+++ app/src/main/java/com/campos/baseconverter/model/ConversionHistory.java
@@ -1,39 +1,57 @@
 package com.campos.baseconverter.model;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.AssetManager;
 import android.util.Log;
+import android.view.animation.AccelerateInterpolator;
+
+import com.campos.baseconverter.util.Tag;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.zip.Inflater;
 
 public class ConversionHistory implements Serializable {
-    private static final String TAG = ConversionHistory.class.getSimpleName();
     private static ConversionHistory history;
 
     public static void init(Activity activity) {
         try {
             history = load(activity);
-            Log.d(TAG, "Loaded history! :D");
-            Log.d(TAG, history.getList().toString());
+            Log.d(Tag.TAG, "Loaded history! :D");
+            Log.d(Tag.TAG, history.getList().toString());
         } catch (IOException | ClassNotFoundException e)  {
-            Log.d(TAG, "Failed to load file.  Generating a new history.");
+            Log.d(Tag.TAG, "Failed to load file.  Generating a new history.");
             history = new ConversionHistory();
         }
     }
 
-    private static ConversionHistory load(Activity activity) throws IOException, ClassNotFoundException {
+    public static void save(Activity a) {
         final String FILE_NAME = "history.dat";
-        FileInputStream fis = activity.openFileInput(FILE_NAME);
+        try {
+            FileOutputStream fis = a.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static ConversionHistory load(Activity a) throws IOException, ClassNotFoundException {
+        final String FILE_NAME = "history.dat";
+        FileInputStream fis = a.openFileInput(FILE_NAME);
         ObjectInputStream ois = new ObjectInputStream(fis);
-        return (ConversionHistory) ois.readObject();
+        ConversionHistory history = (ConversionHistory) ois.readObject();
+        ois.close();
+        return history;
     }
 
     public static ConversionHistory getHistory() {
