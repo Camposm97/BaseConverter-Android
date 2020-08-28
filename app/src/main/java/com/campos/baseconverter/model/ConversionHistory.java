9a9787680b5e79072d765f51997a8ead8ed816d8
Camposm <camposm1205@gmail.com>
2020-08-28 02:34:46 -0400

    Update ConversionHistory.java

--- app/src/main/java/com/campos/baseconverter/model/ConversionHistory.java
+++ app/src/main/java/com/campos/baseconverter/model/ConversionHistory.java
@@ -1,23 +1,42 @@
 package com.campos.baseconverter.model;
 
+import android.util.Log;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.LinkedList;
+import java.util.List;
 
 public class ConversionHistory {
-    private static ConversionHistory convertHistory;
+    private static final String TAG = ConversionHistory.class.getSimpleName();
+    private static ConversionHistory history;
 
     public static void init() {
-        convertHistory = new ConversionHistory();
+        try {
+            history = load();
+        } catch (IOException e) {
+            Log.d(TAG, "Failed to load file.  Generating a new history.");
+            history = new ConversionHistory();
+        }
     }
 
-    public static ConversionHistory getConvertHistory() {
-        return convertHistory;
+    private static ConversionHistory load() throws IOException{
+        ConversionHistory history;
+        FileInputStream fis = new
     }
 
-    private LinkedList<BaseNumber> baseNumbers;
+    public static ConversionHistory getHistory() {
+        return history;
+    }
+
+
+    private List<BaseNumber> list;
 
     private ConversionHistory() {
-        this.baseNumbers = new LinkedList<>();
+        this.list = new LinkedList<>();
     }
 
-
+    public List<BaseNumber> getList() {
+        return list;
+    }
 }
