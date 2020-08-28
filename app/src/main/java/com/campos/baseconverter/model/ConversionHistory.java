6f88fa7df79c3eafbba379621aa19a6b188a40e6
Camposm <camposm1205@gmail.com>
2020-08-28 17:35:21 -0400

    Update ConversionHistory.java

--- app/src/main/java/com/campos/baseconverter/model/ConversionHistory.java
+++ app/src/main/java/com/campos/baseconverter/model/ConversionHistory.java
@@ -1,5 +1,6 @@
 package com.campos.baseconverter.model;
 
+import android.app.Activity;
 import android.content.res.AssetManager;
 import android.util.Log;
 
@@ -8,27 +9,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
+import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.zip.Inflater;
 
-public class ConversionHistory {
+public class ConversionHistory implements Serializable {
     private static final String TAG = ConversionHistory.class.getSimpleName();
     private static ConversionHistory history;
 
-    public static void init(AssetManager assets) {
+    public static void init(Activity activity) {
         try {
-            history = load(assets);
+            history = load(activity);
+            Log.d(TAG, "Loaded history! :D");
+            Log.d(TAG, history.getList().toString());
         } catch (IOException | ClassNotFoundException e)  {
             Log.d(TAG, "Failed to load file.  Generating a new history.");
             history = new ConversionHistory();
         }
     }
 
-    private static ConversionHistory load(AssetManager assets) throws IOException, ClassNotFoundException {
+    private static ConversionHistory load(Activity activity) throws IOException, ClassNotFoundException {
         final String FILE_NAME = "history.dat";
-        Log.d(TAG, assets.toString());
-        InputStream is = assets.open(FILE_NAME);
-        ObjectInputStream ois = new ObjectInputStream(is);
+        FileInputStream fis = activity.openFileInput(FILE_NAME);
+        ObjectInputStream ois = new ObjectInputStream(fis);
         return (ConversionHistory) ois.readObject();
     }
 
@@ -37,7 +41,7 @@ public class ConversionHistory {
     }
 
 
-    private List<BaseNumber> list;
+    private LinkedList<BaseNumber> list;
 
     private ConversionHistory() {
         this.list = new LinkedList<>();
@@ -45,5 +49,9 @@ public class ConversionHistory {
 
     public List<BaseNumber> getList() {
         return list;
+    }
+
+    public void add(BaseNumber baseNumber) {
+        list.addFirst(baseNumber);
     }
 }
