7e9691e26f939b5963d1008ce4ba26626f7fa9e6
Camposm <camposm1205@gmail.com>
2020-08-29 17:19:29 -0400

    Update OptionsActivity.java
    Nothing important

--- app/src/main/java/com/campos/baseconverter/view/activities/OptionsActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activities/OptionsActivity.java
@@ -1,10 +1,13 @@
 package com.campos.baseconverter.view.activities;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.campos.baseconverter.R;
+import com.campos.baseconverter.model.Base;
+import com.campos.baseconverter.model.BaseNumber;
 
 public class OptionsActivity extends AppCompatActivity {
 
