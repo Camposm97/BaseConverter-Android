88f342201736b4fc2c6a9f79c99caa466afdf36f
Camposm <camposm1205@gmail.com>
2020-08-29 17:20:37 -0400

    Update HistoryViewAdapter.java
    Implemented superclass methods

--- app/src/main/java/com/campos/baseconverter/model/HistoryViewAdapter.java
+++ app/src/main/java/com/campos/baseconverter/model/HistoryViewAdapter.java
@@ -1,35 +1,48 @@
 package com.campos.baseconverter.model;
 
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.campos.baseconverter.R;
+import com.campos.baseconverter.util.Tag;
 
 import java.util.List;
 
 public class HistoryViewAdapter extends RecyclerView.Adapter<HistoryViewAdapter.HistoryViewHolder> {
     private List<HistoryViewAdapter> list;
+    private Context context;
 
-    public HistoryViewAdapter(List<HistoryViewAdapter> list) {
+    public HistoryViewAdapter(List<HistoryViewAdapter> list, Context context) {
         this.list = list;
+        this.context = context;
     }
 
     @NonNull
     @Override
     public HistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_view_item, parent, false);
+        HistoryViewHolder holder = new HistoryViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull HistoryViewHolder holder, int position) {
+        Log.d(Tag.TAG, "onBindViewHolder() called");
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class HistoryViewHolder extends RecyclerView.ViewHolder {
@@ -37,6 +50,7 @@ public class HistoryViewAdapter extends RecyclerView.Adapter<HistoryViewAdapter.
 
         public HistoryViewHolder(@NonNull View itemView) {
             super(itemView);
+            itemView.findViewById(R.id.history_item);
         }
     }
 }
