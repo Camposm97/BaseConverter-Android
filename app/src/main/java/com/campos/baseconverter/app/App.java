df7e84f7c630b37500553da05166f4fbc2ae5301
Camposm <camposm1205@gmail.com>
2020-08-29 16:03:44 -0400

    Update App.java
    Calling init() from ConversionHistory here now

--- app/src/main/java/com/campos/baseconverter/app/App.java
+++ app/src/main/java/com/campos/baseconverter/app/App.java
@@ -3,17 +3,25 @@ package com.campos.baseconverter.app;
 import android.app.Application;
 import android.util.Log;
 
-import com.campos.baseconverter.util.Tag;
+import com.campos.baseconverter.model.ConversionHistory;
+
+import static com.campos.baseconverter.util.Tag.TAG;
 
 public class App extends Application {
     public App() {
-        Log.d(Tag.TAG, "Starting App...");
+        Log.d(TAG, "Starting App...");
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        
-        Log.d(Tag.TAG, "onCreate() fired!");
+        ConversionHistory.init(this);
+        Log.d(TAG, "onCreate() fired!");
+    }
+
+    @Override
+    public void onTerminate() {
+        Log.d(TAG, "Goodbye World!");
+        super.onTerminate();
     }
 }
