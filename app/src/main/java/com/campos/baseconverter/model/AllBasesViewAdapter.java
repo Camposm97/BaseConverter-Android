6a2db4eaac50faa6b55dba95cb3d08a852853047
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-30 03:34:19 -0400

    Update AllBasesViewAdapter.java
    Implementing abstract methods

--- app/src/main/java/com/campos/baseconverter/model/AllBasesViewAdapter.java
+++ app/src/main/java/com/campos/baseconverter/model/AllBasesViewAdapter.java
@@ -1,5 +1,40 @@
 package com.campos.baseconverter.model;
 
-public class AllBasesViewAdapter {
-    
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AllBasesViewAdapter extends RecyclerView.Adapter<AllBasesViewHolder> {
+    private Context context;
+    public AllBasesViewAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public AllBasesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AllBasesViewHolder holder, int position) {
+
+    }
+
+    private class AllBasesViewHolder extends RecyclerView.ViewHolder {
+
+
+        public AllBasesViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
