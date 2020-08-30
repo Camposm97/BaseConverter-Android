af79b01b18177c266a3c93ea6c9b3316dcd86441
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-30 03:45:32 -0400

    Update AllBasesViewAdapter.java
    Implemented abstract methods

--- app/src/main/java/com/campos/baseconverter/model/AllBasesViewAdapter.java
+++ app/src/main/java/com/campos/baseconverter/model/AllBasesViewAdapter.java
@@ -1,12 +1,18 @@
 package com.campos.baseconverter.model;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.campos.baseconverter.R;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AllBasesViewAdapter extends RecyclerView.Adapter<AllBasesViewHolder> {
+public class AllBasesViewAdapter extends RecyclerView.Adapter<AllBasesViewAdapter.AllBasesViewHolder> {
     private Context context;
     public AllBasesViewAdapter(Context context) {
         this.context = context;
@@ -15,8 +21,9 @@ public class AllBasesViewAdapter extends RecyclerView.Adapter<AllBasesViewHolder
     @NonNull
     @Override
     public AllBasesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.history_view_item, parent, false);
+        return new AllBasesViewHolder(view);
     }
 
     @Override
@@ -26,14 +33,17 @@ public class AllBasesViewAdapter extends RecyclerView.Adapter<AllBasesViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull AllBasesViewHolder holder, int position) {
-
+        // Put animations here
     }
 
-    private class AllBasesViewHolder extends RecyclerView.ViewHolder {
-
+    public class AllBasesViewHolder extends RecyclerView.ViewHolder {
+        private TextView tv;
+        private EditText tf;
 
         public AllBasesViewHolder(@NonNull View itemView) {
             super(itemView);
+            tv = itemView.findViewById(R.id.output_label);
+            tf = itemView.findViewById(R.id.output_field);
         }
     }
 
