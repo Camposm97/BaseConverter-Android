6d65ef1ff50d77b046beb209198126e5be644c39
Camposm <camposm1205@gmail.com>
2020-08-29 20:33:32 -0400

    Update HistoryViewAdapter.java
    Removed context as an instance variable

--- app/src/main/java/com/campos/baseconverter/model/HistoryViewAdapter.java
+++ app/src/main/java/com/campos/baseconverter/model/HistoryViewAdapter.java
@@ -17,24 +17,23 @@ import java.util.List;
 
 public class HistoryViewAdapter extends RecyclerView.Adapter<HistoryViewAdapter.HistoryViewHolder> {
     private List<BaseNumber> numList;
-    private Context context;
 
-    public HistoryViewAdapter(List<BaseNumber> numList, Context context) {
+    public HistoryViewAdapter(List<BaseNumber> numList) {
         this.numList = numList;
-        this.context = context;
     }
 
     @NonNull
     @Override
     public HistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_view_item, parent, false);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.history_view_item, parent, false);
         return new HistoryViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HistoryViewHolder holder, int position) {
         Log.d(Tag.TAG, "onBindViewHolder() called");
-        holder.tv.setText(numList.get(position).toSpannableString());
+        holder.tv.setText(numList.get(position).toSpanString());
     }
 
     @Override
