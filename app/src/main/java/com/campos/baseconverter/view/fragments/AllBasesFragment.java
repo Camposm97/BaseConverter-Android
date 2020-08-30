ed682ce79677b502a1df8d0503c8f17c12a8f7dd
Camposm <camposm1205@gmail.com>
2020-08-30 15:43:15 -0400

    Update AllBasesFragment.java
    Deleted commented code (old code before using recyclerview)

--- app/src/main/java/com/campos/baseconverter/view/fragments/AllBasesFragment.java
+++ app/src/main/java/com/campos/baseconverter/view/fragments/AllBasesFragment.java
@@ -37,16 +37,13 @@ public class AllBasesFragment extends Fragment {
     private View root;
     private RecyclerView rv;
     private Spinner spinner;
-//    private List<EditText> outputList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         this.root = inflater.inflate(R.layout.fragment_all_bases, container, false);
-//        this.outputList = new LinkedList<>();
         loadRecycler();
         loadSpinner();
-//        loadViewsInRoot();
         return root;
     }
 
@@ -127,82 +124,4 @@ public class AllBasesFragment extends Fragment {
         rv.setAdapter(new BaseNumberViewAdapter(getContext(), list));
         Log.d(TAG, "Cleared fields!");
     }
-
-//    public void showBaseInputDialog(final int position) {
-//        String chosenItem = (String) spinner.getItemAtPosition(position);
-//        BaseInputDialogBuilder dialogBuilder = new BaseInputDialogBuilder(getContext(), chosenItem);
-//        final EditText tfInput = dialogBuilder.getTfInput();
-//        dialogBuilder.setPositiveButton("Convert", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                Base convertFrom = Base.values()[position - 1];
-//                String input = tfInput.getText().toString();
-//                attemptBaseConversion(convertFrom, input);
-//            }
-//        });
-//        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                clearFields();
-//                spinner.setSelection(0);
-//            }
-//        });
-//        dialogBuilder.show();
-//    }
-
-//    public void attemptBaseConversion(Base convertFrom, String input) {
-//        try {
-//            BaseNumber baseNumber = new BaseNumber(convertFrom, input);
-//            BaseConverter baseConverter = new BaseConverter(baseNumber);
-//            ConversionHistory.getHistory().add(baseNumber);
-//            ConversionHistory.save(getActivity());
-//            BaseNumber[] results = baseConverter.getAllResults();
-//            for (int i = 0; i < outputList.size(); i++) {
-//                if (i == 0) {
-//                    outputList.get(i).setText(MyUtils.formatBinStr(results[i].getValue()));
-//                } else {
-//                    outputList.get(i).setText(results[i].getValue());
-//                }
-//            }
-//        } catch (InvalidBaseNumberException e) {
-//            AlertHelper.showInvalidBaseNumInput(getContext());
-//        } finally {
-//            spinner.setSelection(0);
-//        }
-//    }
-
-//    public void loadViewsInRoot() {
-//        LinearLayout layoutOutputField = root.findViewById(R.id.layout_output_field);
-//        ConstraintLayout[] layouts = loadOutputLayouts();
-//        for (int i = 0; i < layouts.length; i++) {
-//            layoutOutputField.addView(layouts[i]);
-//        }
-//    }
-
-//    public ConstraintLayout[] loadOutputLayouts() {
-//        ConstraintLayout[] arr = new ConstraintLayout[Base.values().length];
-//        for (int i = 0; i < arr.length; i++) {
-//            Base base = Base.values()[i];
-//            ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
-//                    LinearLayout.LayoutParams.MATCH_PARENT,
-//                    LinearLayout.LayoutParams.WRAP_CONTENT);
-//            layoutParams.setMargins(0, 0, 0, 10);
-//
-//            arr[i] = (ConstraintLayout) root.inflate(getContext(), R.layout.base_output_layout, null);
-//            arr[i].setLayoutParams(layoutParams);
-//
-//            TextView lbl = (TextView) arr[i].getChildAt(0);
-//            lbl.setText(Base.toTitle(base));
-//
-//            EditText tf = (EditText) arr[i].getChildAt(1);
-//            outputList.add(tf);
-//        }
-//        return arr;
-//    }
-
-//    public void clearFields() {
-//        for (EditText tf : outputList) {
-//            tf.getText().clear();
-//        }
-//    }
 }
