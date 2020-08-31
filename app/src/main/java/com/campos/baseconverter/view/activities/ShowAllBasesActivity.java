e4c82206b1f27724d9ec1b43d9e66803a9d13de7
Camposm <camposm1205@gmail.com>
2020-08-30 23:02:33 -0400

    Update ShowAllBasesActivity.java
    Implementing ShowAllBasesActivity...

--- app/src/main/java/com/campos/baseconverter/view/activities/ShowAllBasesActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activities/ShowAllBasesActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.campos.baseconverter.R;
+import com.campos.baseconverter.model.BaseNumber;
+import static com.campos.baseconverter.util.Tag.TAG;
 
 public class ShowAllBasesActivity extends AppCompatActivity {
     private RecyclerView rv;
@@ -14,5 +17,7 @@ public class ShowAllBasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_all_bases);
+        BaseNumber baseNumber = (BaseNumber) this.getIntent().getSerializableExtra("target");
+        Log.d(TAG, "Showing all conversions for: " + baseNumber.toString());
     }
 }
