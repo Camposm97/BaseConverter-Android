39c3d8bf8e1c9a6dfb542d551fb244a9a65a3cd6
Camposm <camposm1205@gmail.com>
2020-08-30 23:53:39 -0400

    Update ShowAllBasesActivity.java
    Removed debugging tag

--- app/src/main/java/com/campos/baseconverter/view/activities/ShowAllBasesActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activities/ShowAllBasesActivity.java
@@ -1,19 +1,16 @@
 package com.campos.baseconverter.view.activities;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.util.Log;
 
 import com.campos.baseconverter.R;
 import com.campos.baseconverter.model.BaseConverter;
 import com.campos.baseconverter.model.BaseNumber;
 import com.campos.baseconverter.model.BaseNumberViewAdapter;
 import com.campos.baseconverter.model.InvalidBaseNumberException;
-
-import static com.campos.baseconverter.util.Tag.TAG;
 
 public class ShowAllBasesActivity extends AppCompatActivity {
     private BaseNumber chosenNum;
@@ -35,7 +32,6 @@ public class ShowAllBasesActivity extends AppCompatActivity {
         } catch (InvalidBaseNumberException e) {
             e.printStackTrace();
         }
-        Log.d(TAG, "Showing all conversions for: " + chosenNum.toString());
     }
 
     public void loadRecycler() {
