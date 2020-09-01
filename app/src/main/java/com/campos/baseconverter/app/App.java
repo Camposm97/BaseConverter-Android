2a4015df7c8e74d5ad852c41032a01115d222d50
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-08-31 23:24:10 -0400

    Update App.java
    Found a class method that can help in the long run

--- app/src/main/java/com/campos/baseconverter/app/App.java
+++ app/src/main/java/com/campos/baseconverter/app/App.java
@@ -5,6 +5,8 @@ import android.util.Log;
 
 import com.campos.baseconverter.model.ConversionHistory;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 import static com.campos.baseconverter.util.Tag.TAG;
 
 public class App extends Application {
@@ -15,6 +17,8 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // This is good to know! :D
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         ConversionHistory.init(this);
     }
 }
