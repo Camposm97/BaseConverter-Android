b0425e1742b90b431463b80387de330a453bbf35
Camposm <camposm1205@gmail.com>
2020-09-01 19:12:24 -0400

    Update OptionsActivity.java
    I'm programming over here

--- app/src/main/java/com/campos/baseconverter/view/activities/OptionsActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activities/OptionsActivity.java
@@ -1,6 +1,7 @@
 package com.campos.baseconverter.view.activities;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,8 +22,23 @@ public class OptionsActivity extends AppCompatActivity {
 
     public void chooseTheme(View v) {
         Log.d(TAG, "Choosing Theme...");
+//        String[] choices = getResources().getStringArray(R.array.themes);
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        
+        alert.setSingleChoiceItems(R.array.themes, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d(TAG, "which=" + which);
+            }
+        });
+//        final View view = getLayoutInflater().inflate(R.layout.theme_chooser_layout, null);
+//        alert.setView(view);
+//        alert.setPositiveButton(R.string.app_name, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                Log.d(TAG, "which=" + which);
+//            }
+//        });
+//        alert.setNegativeButton(R.string.bt_no, null);
         alert.show();
     }
 }
