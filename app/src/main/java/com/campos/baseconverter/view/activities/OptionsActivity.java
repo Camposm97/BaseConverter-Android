faa6f1552f4ca3706733cfe952c812ee0c1c9dbb
Camposm <camposm1205@gmail.com>
2020-09-01 19:24:13 -0400

    Update OptionsActivity.java
    Successfully implmented function for choosing your own theme

--- app/src/main/java/com/campos/baseconverter/view/activities/OptionsActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activities/OptionsActivity.java
@@ -7,12 +7,14 @@ import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.campos.baseconverter.R;
 
 import static com.campos.baseconverter.util.Tag.TAG;
 
 public class OptionsActivity extends AppCompatActivity {
+    private int themeChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +24,32 @@ public class OptionsActivity extends AppCompatActivity {
 
     public void chooseTheme(View v) {
         Log.d(TAG, "Choosing Theme...");
-//        String[] choices = getResources().getStringArray(R.array.themes);
+        final String[] themeArr = getResources().getStringArray(R.array.themes);
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle(R.string.str_theme);
         alert.setSingleChoiceItems(R.array.themes, -1, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Log.d(TAG, "which=" + which);
+                themeChoice = which;
             }
         });
-//        final View view = getLayoutInflater().inflate(R.layout.theme_chooser_layout, null);
-//        alert.setView(view);
-//        alert.setPositiveButton(R.string.app_name, new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                Log.d(TAG, "which=" + which);
-//            }
-//        });
-//        alert.setNegativeButton(R.string.bt_no, null);
+        alert.setPositiveButton(R.string.bt_yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (themeChoice) {
+                    case 0:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        break;
+                    case 1:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        break;
+                    case 2:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                        break;
+                }
+            }
+        });
+        alert.setNegativeButton(R.string.bt_no, null);
         alert.show();
     }
 }
