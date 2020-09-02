24a3305e4df162d1561bb3c58c6dea8dad8fdbee
Camposm <camposm1205@gmail.com>
2020-09-01 20:29:07 -0400

    Update OptionsActivity.java
    Re-added yes and no buttons to fix a leak from the theme chooser dialog

--- app/src/main/java/com/campos/baseconverter/view/activities/OptionsActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activities/OptionsActivity.java
@@ -1,6 +1,8 @@
 package com.campos.baseconverter.view.activities;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
@@ -11,10 +13,14 @@ import com.campos.baseconverter.R;
 import com.campos.baseconverter.model.ThemeChooser;
 
 public class OptionsActivity extends AppCompatActivity {
+    private Context c;
+    private int code;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+        this.c = this;
     }
 
     public void chooseTheme(View v) {
@@ -23,9 +29,17 @@ public class OptionsActivity extends AppCompatActivity {
         dialogBuilder.setSingleChoiceItems(R.array.themes, -1, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                ThemeChooser.setTheme(which);
+                code = which;
             }
         });
+        dialogBuilder.setPositiveButton(R.string.bt_yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ThemeChooser.setTheme(c, code);
+            }
+        });
+        dialogBuilder.setNegativeButton(R.string.bt_no, null);
+        dialogBuilder.setCancelable(false);
         dialogBuilder.show();
     }
 }
