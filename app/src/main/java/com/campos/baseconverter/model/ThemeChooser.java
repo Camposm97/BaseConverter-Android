b28c366fa20e9fc09843c424afae71a87216c3a9
Camposm <camposm1205@gmail.com>
2020-09-01 20:31:07 -0400

    Update ThemeChooser.java
    Implemented methods to set, save, and load theme

--- app/src/main/java/com/campos/baseconverter/model/ThemeChooser.java
+++ app/src/main/java/com/campos/baseconverter/model/ThemeChooser.java
@@ -1,12 +1,22 @@
 package com.campos.baseconverter.model;
 
+import android.content.Context;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatDelegate;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static com.campos.baseconverter.util.Tag.TAG;
 
 public class ThemeChooser {
     private static final String FILE_NAME = "theme.dat";
 
-    public static void setTheme(int which) {
-        switch (which) {
+    public static void setTheme(Context c, int code) {
+        switch (code) {
             case 0:
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 break;
@@ -17,7 +27,31 @@ public class ThemeChooser {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
                 break;
         }
+        saveTheme(c, code);
     }
 
+    private static void saveTheme(Context c, int code) {
+        try {
+            FileOutputStream fis = c.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+            fis.write(code);
+            fis.close();
+            Log.d(TAG, "Successfully saved theme!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
+    public static int load(Context c) {
+        int code = -1;
+        try {
+            FileInputStream fis = c.openFileInput(FILE_NAME);
+            code = fis.read();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            setTheme(c, code);
+            return code;
+        }
+    }
 }
