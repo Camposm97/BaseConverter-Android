04161e9674c4590bf89aca35381de97495fdc43e
Camposm <camposm1205@gmail.com>
2020-09-01 20:15:33 -0400

    Update OptionsActivity.java
    Calling ThemeChooser

--- app/src/main/java/com/campos/baseconverter/view/activities/OptionsActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activities/OptionsActivity.java
@@ -3,15 +3,12 @@ package com.campos.baseconverter.view.activities;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 import com.campos.baseconverter.R;
-
-import static com.campos.baseconverter.util.Tag.TAG;
+import com.campos.baseconverter.model.ThemeChooser;
 
 public class OptionsActivity extends AppCompatActivity {
     @Override
@@ -26,17 +23,7 @@ public class OptionsActivity extends AppCompatActivity {
         dialogBuilder.setSingleChoiceItems(R.array.themes, -1, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                switch (which) {
-                    case 0:
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                        break;
-                    case 1:
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                        break;
-                    case 2:
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-                        break;
-                }
+                ThemeChooser.setTheme(which);
             }
         });
         dialogBuilder.show();
