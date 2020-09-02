01d13130bf91352d8604d349ee8c920faba001e0
Camposm <camposm1205@gmail.com>
2020-09-01 20:37:17 -0400

    Update ThemeChooser.java
    Successfully implemented methods for ThemeChooser (class methods)

--- app/src/main/java/com/campos/baseconverter/model/ThemeChooser.java
+++ app/src/main/java/com/campos/baseconverter/model/ThemeChooser.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 import static com.campos.baseconverter.util.Tag.TAG;
 
@@ -16,37 +18,46 @@ public class ThemeChooser {
     private static final String FILE_NAME = "theme.dat";
 
     public static void setTheme(Context c, int code) {
+        Log.d(TAG, "Setting theme...");
+        Log.d(TAG, "code=" + code);
         switch (code) {
             case 0:
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                saveTheme(c, code);
                 break;
             case 1:
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                saveTheme(c, code);
                 break;
             case 2:
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                saveTheme(c, code);
                 break;
+            default:
+                Log.d(TAG, "Invalid theme code!");
         }
-        saveTheme(c, code);
     }
 
     private static void saveTheme(Context c, int code) {
         try {
             FileOutputStream fis = c.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
-            fis.write(code);
-            fis.close();
+            ObjectOutputStream oos = new ObjectOutputStream(fis);
+            oos.writeInt(code);
+            oos.close();
             Log.d(TAG, "Successfully saved theme!");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     public static int load(Context c) {
         int code = -1;
         try {
             FileInputStream fis = c.openFileInput(FILE_NAME);
-            code = fis.read();
-            fis.close();
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            code = ois.readInt();
+            ois.close();
+            Log.d(TAG, "Successfully loaded theme!");
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
