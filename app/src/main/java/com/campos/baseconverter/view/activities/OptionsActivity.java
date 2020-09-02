a0706add943aae42d037c045b3f82a7a8625f9b9
Camposm <camposm1205@gmail.com>
2020-09-01 20:09:32 -0400

    Update OptionsActivity.java
    Deleted commented code (no longer needed)

--- app/src/main/java/com/campos/baseconverter/view/activities/OptionsActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activities/OptionsActivity.java
@@ -14,8 +14,6 @@ import com.campos.baseconverter.R;
 import static com.campos.baseconverter.util.Tag.TAG;
 
 public class OptionsActivity extends AppCompatActivity {
-//    private int themeChoice;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +21,11 @@ public class OptionsActivity extends AppCompatActivity {
     }
 
     public void chooseTheme(View v) {
-        Log.d(TAG, "Choosing Theme...");
-        final String[] themeArr = getResources().getStringArray(R.array.themes);
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle(R.string.str_theme);
-        alert.setSingleChoiceItems(R.array.themes, -1, new DialogInterface.OnClickListener() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle(R.string.str_theme);
+        dialogBuilder.setSingleChoiceItems(R.array.themes, -1, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-//                themeChoice = which;
                 switch (which) {
                     case 0:
                         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
@@ -44,23 +39,6 @@ public class OptionsActivity extends AppCompatActivity {
                 }
             }
         });
-//        alert.setPositiveButton(R.string.bt_yes, new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                switch (themeChoice) {
-//                    case 0:
-//                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-//                        break;
-//                    case 1:
-//                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-//                        break;
-//                    case 2:
-//                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-//                        break;
-//                }
-//            }
-//        });
-//        alert.setNegativeButton(R.string.bt_no, null);
-        alert.show();
+        dialogBuilder.show();
     }
 }
