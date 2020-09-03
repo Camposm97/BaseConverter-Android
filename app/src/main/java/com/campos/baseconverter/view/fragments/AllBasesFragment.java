764c6efa5a7ab25b672f6a1145d68a23f30f53b8
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-03 15:02:13 -0400

    Update AllBasesFragment.java

--- app/src/main/java/com/campos/baseconverter/view/fragments/AllBasesFragment.java
+++ app/src/main/java/com/campos/baseconverter/view/fragments/AllBasesFragment.java
@@ -19,7 +19,7 @@ import com.campos.baseconverter.model.BaseConverter;
 import com.campos.baseconverter.model.BaseInputDialogBuilder;
 import com.campos.baseconverter.model.BaseNumber;
 import com.campos.baseconverter.model.BaseNumberViewAdapter;
-import com.campos.baseconverter.model.ConversionHistory;
+import com.campos.baseconverter.model.UserHistory;
 import com.campos.baseconverter.model.InvalidBaseNumberException;
 import com.campos.baseconverter.util.AlertHelper;
 
@@ -92,9 +92,9 @@ public class AllBasesFragment extends Fragment {
     public void startBaseConversion(BaseNumber baseNumber) {
         try {
             BaseConverter baseConverter = new BaseConverter(baseNumber);
-            ConversionHistory.getHistory().add(baseNumber);
-            ConversionHistory.save(getActivity());
             BaseNumber[] resultsArr = baseConverter.getAllResults();
+            UserHistory.getHistory().add(baseNumber, resultsArr);
+            UserHistory.save(getActivity());
             rv.setAdapter(new BaseNumberViewAdapter(getContext(), resultsArr));
         } catch (InvalidBaseNumberException e) {
             AlertHelper.showInvalidBaseNumInput(getContext());
