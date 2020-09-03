c24b4cbcf01f621f1a40464d77b3118bea7476f8
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-03 15:00:15 -0400

    Update App.java

--- app/src/main/java/com/campos/baseconverter/app/App.java
+++ app/src/main/java/com/campos/baseconverter/app/App.java
@@ -1,11 +1,9 @@
 package com.campos.baseconverter.app;
 
 import android.app.Application;
-import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.util.Log;
 
-import com.campos.baseconverter.model.ConversionHistory;
+import com.campos.baseconverter.model.UserHistory;
 import com.campos.baseconverter.model.NumSchemeChooser;
 import com.campos.baseconverter.model.ThemeChooser;
 
@@ -24,7 +22,7 @@ public class App extends Application {
         super.onCreate();
         themeCode = ThemeChooser.load(this);
         numSchemeCode = NumSchemeChooser.load(this);
-        ConversionHistory.init(this);
+        UserHistory.init(this);
         /*
         SharedPreferences - something I can use in the future instead of saving dat files
         I can make the program run faster if I save history with not only the BaseNumber, but
