fc8ce867a81f50c041969c7cb36d0d735c7cde74
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-04 18:20:47 -0400

    Update NumSchemeChooser.java
    Deleting commented code

--- app/src/main/java/com/campos/baseconverter/model/NumSchemeChooser.java
+++ app/src/main/java/com/campos/baseconverter/model/NumSchemeChooser.java
@@ -2,28 +2,10 @@ package com.campos.baseconverter.model;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.util.Log;
 
 import com.campos.baseconverter.app.App;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
-import static com.campos.baseconverter.util.Tag.TAG;
-
 public class NumSchemeChooser {
-//    private static final String FILE_NAME = "num_scheme.dat";
-
-//    public static void setScheme(Context c, int code) {
-//        Log.d(TAG, "Setting Number Scheme...");
-//        Log.d(TAG, "code=" + code);
-//        App.numSchemeCode = code;
-//        save(c, code);
-//    }
-
     public static void save(Context c, int code) {
         SharedPreferences pref = c.getSharedPreferences(App.SETTINGS_FILE, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
