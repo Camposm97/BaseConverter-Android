e32fa0c60b51653c26f6827d0fe50bc7ef1d0f98
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-04 18:24:20 -0400

    Update AlertUtils.java
    Renamed class

--- app/src/main/java/com/campos/baseconverter/util/AlertUtils.java
+++ app/src/main/java/com/campos/baseconverter/util/AlertUtils.java
@@ -5,7 +5,7 @@ import android.widget.Toast;
 
 import com.campos.baseconverter.R;
 
-public class AlertHelper {
+public class AlertUtils {
     public static void showInvalidBaseNumInput(Context c) {
         Toast.makeText(c, R.string.invalid_base_num_message, Toast.LENGTH_SHORT).show();
     }
