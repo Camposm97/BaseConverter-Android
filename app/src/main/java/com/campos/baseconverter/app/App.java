02a63f1e593bd6ee2f5f16573fbae4f1a55f738e
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-04 14:59:12 -0400

    Update App.java
    Setting up variables and deleting comments for replacing old methods for loading and saving using SharedPreference

--- app/src/main/java/com/campos/baseconverter/app/App.java
+++ app/src/main/java/com/campos/baseconverter/app/App.java
@@ -10,13 +10,11 @@ import com.campos.baseconverter.model.UserHistory;
 import static com.campos.baseconverter.util.Tag.TAG;
 
 public class App extends Application {
-    private static final String PREF_FILE = "options.properties";
+    public static final String PREF_FILE = "settings";
+    public static final String THEME_KEY = "theme_key";
+    public static final String NUM_SCHEME_KEY = "num_scheme_key";
     public static int themeCode = -1;
     public static int numSchemeCode = -1;
-
-    public App() {
-        Log.d(TAG, "Starting App...");
-    }
 
     @Override
     public void onCreate() {
@@ -24,14 +22,5 @@ public class App extends Application {
         themeCode = ThemeChooser.load(this);
         numSchemeCode = NumSchemeChooser.load(this);
         UserHistory.init(this);
-        /*
-        SharedPreferences - something I can use in the future instead of saving dat files
-        I can make the program run faster if I save history with not only the BaseNumber, but
-        with all of it's results: BaseNumber[]
-         */
-    }
-
-    public static String getPrefFile() {
-        return PREF_FILE;
     }
 }
