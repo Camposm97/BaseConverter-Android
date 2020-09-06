296c052fddb61ed80570997890f90d7c0d84370e
Camposm <camposm1205@gmail.com>
2020-09-05 22:43:07 -0400

    Update SinglePrecisionConverter.java

--- app/src/main/java/com/campos/baseconverter/model/SinglePrecisionConverter.java
+++ app/src/main/java/com/campos/baseconverter/model/SinglePrecisionConverter.java
@@ -10,12 +10,18 @@ public class SinglePrecisionConverter {
         this.input = input;
     }
 
-    public void convertToBin() {
+    public void convertToBin() throws InvalidBaseNumberException {
         if (input.getBase().equals(Base.BASE_10)) {
             BigDecimal value = new BigDecimal(input.getValue());
             BigDecimal[] arr = value.divideAndRemainder(BigDecimal.ONE);
-            System.out.println(arr[0]);
-            System.out.println(arr[1]);
+//            System.out.println(arr[0]);
+//            System.out.println(arr[1]);
+
+            BaseNumber num = new BaseNumber(input.getBase(), arr[0].toBigInteger().toString());
+            BaseConverter baseConverter = new BaseConverter(num, Base.BASE_2);
+            BaseNumber result1 = baseConverter.convert(); //  Result before '.'
+
+            
         }
     }
 }
