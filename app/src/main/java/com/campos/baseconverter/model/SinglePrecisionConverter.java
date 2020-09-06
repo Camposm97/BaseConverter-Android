a0d7c6fcb1d782d9b06f4165d8f804184e0916a4
Camposm <camposm1205@gmail.com>
2020-09-05 23:04:56 -0400

    Update SinglePrecisionConverter.java
    Deleting commented code (no longer needed)

--- app/src/main/java/com/campos/baseconverter/model/SinglePrecisionConverter.java
+++ app/src/main/java/com/campos/baseconverter/model/SinglePrecisionConverter.java
@@ -18,8 +18,6 @@ public class SinglePrecisionConverter {
         if (input.getBase().equals(Base.BASE_10)) {
             BigDecimal value = new BigDecimal(input.getValue());
             BigDecimal[] arr = value.divideAndRemainder(BigDecimal.ONE);
-//            System.out.println(arr[0]);
-//            System.out.println(arr[1]);
 
             BaseNumber num = new BaseNumber(input.getBase(), arr[0].toBigInteger().toString());
             BaseConverter baseConverter = new BaseConverter(num, Base.BASE_2);
@@ -30,22 +28,16 @@ public class SinglePrecisionConverter {
             final int RADIX = Base.BASE_2.getRadix();
             final int LIMIT = 23;
             String result2 = "";
-//            List<String> list = new LinkedList<>();
             for (int i = 0; i < LIMIT; i++) {
                 decimals = decimals.multiply(BigDecimal.valueOf(RADIX));
                 BigInteger bit = decimals.divide(BigDecimal.ONE).toBigInteger();
-//                System.out.print(decimals + " : ");
-//                System.out.println(BigDecimal.ONE.compareTo(decimals));
                 if (BigDecimal.ONE.compareTo(decimals) <= 0) { // Is greater than 1
                     decimals = decimals.subtract(new BigDecimal(bit));
                     result2 = result2 + bit.toString();
-//                    list.add(bit.toString());
                 } else { // Is less than one
                     result2 = result2 + bit.toString();
-//                    list.add(bit.toString());
                 }
             }
-            System.out.println(result2);
         }
     }
 }
