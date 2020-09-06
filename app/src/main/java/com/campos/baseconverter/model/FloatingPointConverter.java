999e7d5c1152db60ad8d2254a4b2863343f42d93
Camposm <camposm1205@gmail.com>
2020-09-05 22:21:21 -0400

    Update FloatingPointConverter.java

--- app/src/main/java/com/campos/baseconverter/model/FloatingPointConverter.java
+++ app/src/main/java/com/campos/baseconverter/model/FloatingPointConverter.java
@@ -2,5 +2,9 @@ package com.campos.baseconverter.model;
 
 
 public class FloatingPointConverter {
-    
+    private BaseNumber input;
+
+    public FloatingPointConverter(BaseNumber input) {
+        this.input = input;
+    }
 }
