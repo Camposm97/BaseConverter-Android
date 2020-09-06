e107c07c2d1a862be87f6d3f9a3fca051451e61e
Camposm <camposm1205@gmail.com>
2020-09-05 22:43:09 -0400

    Update SinglePrecisionConverterTest.java

--- app/src/test/java/com/campos/baseconverter/SinglePrecisionConverterTest.java
+++ app/src/test/java/com/campos/baseconverter/SinglePrecisionConverterTest.java
@@ -2,13 +2,14 @@ package com.campos.baseconverter;
 
 import com.campos.baseconverter.model.Base;
 import com.campos.baseconverter.model.BaseNumber;
+import com.campos.baseconverter.model.InvalidBaseNumberException;
 import com.campos.baseconverter.model.SinglePrecisionConverter;
 
 import org.junit.Test;
 
 public class SinglePrecisionConverterTest {
     @Test
-    public void convertTest() {
+    public void convertTest() throws InvalidBaseNumberException {
         BaseNumber number = new BaseNumber(Base.BASE_10, "15.125");
         SinglePrecisionConverter precisionConverter = new SinglePrecisionConverter(number);
         precisionConverter.convertToBin();
