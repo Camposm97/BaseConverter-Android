768976f5ad17f7500572007f1f428f807eac2e3c
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-06 23:55:19 -0400

    Update SinglePrecisionConverter.java
    Implementing calcWholePart()

--- app/src/main/java/com/campos/baseconverter/model/SinglePrecisionConverter.java
+++ app/src/main/java/com/campos/baseconverter/model/SinglePrecisionConverter.java
@@ -19,14 +19,15 @@ public class SinglePrecisionConverter {
         if (input.getBase().equals(Base.BASE_10)) {
             BigDecimal value = new BigDecimal(input.getValue());
             BigDecimal[] arr = value.divideAndRemainder(BigDecimal.ONE);
-            
+
+            String result1 = calcWholePart(arr[0]);
 //            BaseNumber num = new BaseNumber(input.getBase(), arr[0].toBigInteger().toString());
 //            BaseConverter baseConverter = new BaseConverter(num, Base.BASE_2);
 //            BaseNumber result1 = baseConverter.convert(); //  Result before '.'
 
             BigDecimal decimals = arr[1];
             String result2 = calcFractionalPart(decimals);
-            result = result1.getValue() + "." + result2;
+            result = result1 + "." + result2;
         }
         return result;
     }
