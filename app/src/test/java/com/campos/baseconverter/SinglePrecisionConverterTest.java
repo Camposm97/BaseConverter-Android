4f854793962777b7dd7a858a197116375e3ecef3
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-07 12:29:47 -0400

    Update SinglePrecisionConverterTest.java
    Testing while implementing convert()

--- app/src/test/java/com/campos/baseconverter/SinglePrecisionConverterTest.java
+++ app/src/test/java/com/campos/baseconverter/SinglePrecisionConverterTest.java
@@ -9,11 +9,18 @@ import org.junit.Test;
 
 public class SinglePrecisionConverterTest {
     @Test
-    public void convertTest() throws InvalidBaseNumberException {
+    public void convertToBinStrTest() throws InvalidBaseNumberException {
         BaseNumber number = new BaseNumber(Base.BASE_10, "15.125");
         SinglePrecisionConverter precisionConverter = new SinglePrecisionConverter(number);
         String result = precisionConverter.convertToBinStr();
         System.out.println("before: " + number.getValue());
         System.out.println("after: " + result);
+    }
+
+    @Test
+    public void convertTest() throws InvalidBaseNumberException {
+        BaseNumber number = new BaseNumber(Base.BASE_3, "1111.001");
+        SinglePrecisionConverter precisionConverter = new SinglePrecisionConverter(number);
+        precisionConverter.convertToDec();
     }
 }
