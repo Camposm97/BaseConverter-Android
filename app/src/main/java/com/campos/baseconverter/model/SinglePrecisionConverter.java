1b7a36c71c074d34fc60bedc438f03dbb97ab5a1
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-07 00:07:00 -0400

    Update SinglePrecisionConverter.java
    Deleted commented code

--- app/src/main/java/com/campos/baseconverter/model/SinglePrecisionConverter.java
+++ app/src/main/java/com/campos/baseconverter/model/SinglePrecisionConverter.java
@@ -20,14 +20,9 @@ public class SinglePrecisionConverter {
             BigDecimal value = new BigDecimal(input.getValue());
             BigDecimal[] arr = value.divideAndRemainder(BigDecimal.ONE);
 
-            String result1 = calcWholePart(arr[0]);
-//            BaseNumber num = new BaseNumber(input.getBase(), arr[0].toBigInteger().toString());
-//            BaseConverter baseConverter = new BaseConverter(num, Base.BASE_2);
-//            BaseNumber result1 = baseConverter.convert(); //  Result before '.'
-
-            BigDecimal decimals = arr[1];
-            String result2 = calcFractionalPart(decimals);
-            result = result1 + "." + result2;
+            String wholePart = calcWholePart(arr[0]);
+            String fractionalPart = calcFractionalPart(arr[1]);
+            result = wholePart + "." + fractionalPart;
         }
         return result;
     }
