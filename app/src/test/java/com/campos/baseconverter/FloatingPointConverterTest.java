e2a29675bce8cae84e2e4ed2fe4e7caaa4861331
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-10 15:06:35 -0400

    Create FloatingPointConverterTest.java

---
+++ app/src/test/java/com/campos/baseconverter/FloatingPointConverterTest.java
@@ -0,0 +1,27 @@
+package com.campos.baseconverter;
+
+import com.campos.baseconverter.model.Base;
+import com.campos.baseconverter.model.BaseNumber;
+import com.campos.baseconverter.model.InvalidBaseNumberException;
+import com.campos.baseconverter.model.FloatingPointerConverter;
+
+import org.junit.Test;
+
+public class FloatingPointConverterTest {
+    @Test
+    public void convertToBinStrTest() throws InvalidBaseNumberException {
+        BaseNumber number = new BaseNumber(Base.BASE_10, "15.125");
+        FloatingPointerConverter precisionConverter = new FloatingPointerConverter(number);
+        String result = precisionConverter.convertToBinStr();
+        System.out.println("before: " + number.getValue());
+        System.out.println("after: " + result);
+    }
+
+    @Test
+    public void convertTest() throws InvalidBaseNumberException {
+        BaseNumber number = new BaseNumber(Base.BASE_16, "A.A");
+        FloatingPointerConverter precisionConverter = new FloatingPointerConverter(number);
+        String result = precisionConverter.convertToDec();
+        System.out.println(result);
+    }
+}
