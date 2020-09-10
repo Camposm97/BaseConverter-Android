a119955d5e4a822918be07c5392ddfafa139afcc
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-10 15:28:34 -0400

    Update FloatingPointConverterTest.java

--- app/src/test/java/com/campos/baseconverter/FloatingPointConverterTest.java
+++ app/src/test/java/com/campos/baseconverter/FloatingPointConverterTest.java
@@ -28,8 +28,9 @@ public class FloatingPointConverterTest {
     @Test
     public void convertTest() throws InvalidBaseNumberException{
         BaseNumber number = new BaseNumber(Base.BASE_10, "15.125");
-        Base convertTo = Base.BASE_2;
+        Base convertTo = Base.BASE_10;
         FloatingPointerConverter converter = new FloatingPointerConverter(number);
         BaseNumber result = converter.convert(convertTo);
+        System.out.println(result);
     }
 }
