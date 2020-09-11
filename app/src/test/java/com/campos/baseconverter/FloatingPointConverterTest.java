c48db2143e390aacbb2611da6488eb43beff02b7
Camposm <camposm1205@gmail.com>
2020-09-11 01:36:37 -0400

    Update FloatingPointConverterTest.java
    Creating tests

--- app/src/test/java/com/campos/baseconverter/FloatingPointConverterTest.java
+++ app/src/test/java/com/campos/baseconverter/FloatingPointConverterTest.java
@@ -2,22 +2,27 @@ package com.campos.baseconverter;
 
 import com.campos.baseconverter.model.Base;
 import com.campos.baseconverter.model.BaseNumber;
-import com.campos.baseconverter.model.InvalidBaseNumberException;
 import com.campos.baseconverter.model.FloatingPointerConverter;
+import com.campos.baseconverter.model.InvalidBaseNumberException;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class FloatingPointConverterTest {
     @Test
-    public void convertTest() throws InvalidBaseNumberException {
-        BaseNumber number = new BaseNumber(Base.BASE_10, "15.125");
-        System.out.println(number);
-        Base convertTo = Base.BASE_2;
-        FloatingPointerConverter converter = new FloatingPointerConverter(number);
+    public void convertTest1() throws InvalidBaseNumberException {
+        final String expectedValue1 = "1111.001";
+        final String expectedValue2 = "15.125";
+        BaseNumber input = new BaseNumber(Base.BASE_10, "15.125");
+        Base convertTo1 = Base.BASE_2;
+        Base convertTo2 = Base.BASE_10;
+        FloatingPointerConverter converter = new FloatingPointerConverter(input);
         converter.setScale(3);
-        BaseNumber result = converter.convert(convertTo);
-        System.out.println(result);
-        FloatingPointerConverter converter1 = new FloatingPointerConverter(result);
-        System.out.println(converter1.convert(Base.BASE_10));
+        BaseNumber result1 = converter.convert(convertTo1);
+        converter.setInput(result1);
+        BaseNumber result2 = converter.convert(convertTo2);
+        assertEquals(expectedValue1, result1.getValue());
+        assertEquals(expectedValue2, result2.getValue());
     }
 }
