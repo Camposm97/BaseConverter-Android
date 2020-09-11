01bd9afc5f45be2d8906368e7fe64d783c91d98b
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-10 20:08:24 -0400

    Update FloatingPointConverterTest.java
    Deleted old tests (no longer needed)

--- app/src/test/java/com/campos/baseconverter/FloatingPointConverterTest.java
+++ app/src/test/java/com/campos/baseconverter/FloatingPointConverterTest.java
@@ -11,23 +11,6 @@ import java.math.BigDecimal;
 
 public class FloatingPointConverterTest {
     @Test
-    public void convertToBinTest() throws InvalidBaseNumberException {
-        BaseNumber number = new BaseNumber(Base.BASE_10, "15.125");
-        FloatingPointerConverter converter = new FloatingPointerConverter(number);
-        BaseNumber result = converter.convert(Base.BASE_2);
-        System.out.println("before: " + number);
-        System.out.println("after: " + result);
-    }
-
-    @Test
-    public void convertToDecTest() throws InvalidBaseNumberException {
-        BaseNumber number = new BaseNumber(Base.BASE_16, "A.A");
-        FloatingPointerConverter converter = new FloatingPointerConverter(number);
-        String result = converter.convertToDec();
-        System.out.println(result);
-    }
-
-    @Test
     public void convertTest() throws InvalidBaseNumberException{
         BaseNumber number = new BaseNumber(Base.BASE_10, "15.125");
         System.out.println(number);
