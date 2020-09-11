f48cc58b56fae05be4d8c6e3c2217641628ec824
Camposm <camposm1205@gmail.com>
2020-09-11 01:39:16 -0400

    Update FloatingPointConverterTest.java
    Refactoring variables

--- app/src/test/java/com/campos/baseconverter/FloatingPointConverterTest.java
+++ app/src/test/java/com/campos/baseconverter/FloatingPointConverterTest.java
@@ -12,23 +12,23 @@ import static org.junit.Assert.assertEquals;
 public class FloatingPointConverterTest {
     @Test
     public void convertTest1() throws InvalidBaseNumberException {
-        String expectedValue1 = "1111.001";
+        String expectedValue = "1111.001";
         BaseNumber input = new BaseNumber(Base.BASE_10, "15.125");
-        Base convertTo1 = Base.BASE_2;
+        Base convertTo = Base.BASE_2;
         FloatingPointerConverter converter = new FloatingPointerConverter(input);
         converter.setScale(3);
-        BaseNumber result1 = converter.convert(convertTo1);
-        assertEquals(expectedValue1, result1.getValue());
+        BaseNumber result = converter.convert(convertTo);
+        assertEquals(expectedValue, result.getValue());
     }
 
     @Test
     public void convertTest2() throws InvalidBaseNumberException {
-        String expectedValue2 = "15.125";
+        String expectedValue = "15.125";
         BaseNumber input = new BaseNumber(Base.BASE_2, "1111.001");
-        Base convertTo2 = Base.BASE_10;
+        Base convertTo = Base.BASE_10;
         FloatingPointerConverter converter = new FloatingPointerConverter(input);
         converter.setScale(3);
-        BaseNumber result2 = converter.convert(convertTo2);
-        assertEquals(expectedValue2, result2.getValue());
+        BaseNumber result = converter.convert(convertTo);
+        assertEquals(expectedValue, result.getValue());
     }
 }
