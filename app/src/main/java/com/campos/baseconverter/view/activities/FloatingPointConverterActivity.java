d09f12bdb42621740cc4d0a203578b7d3f469b79
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-17 23:21:14 -0400

    Create FloatingPointConverterActivity.java

---
+++ app/src/main/java/com/campos/baseconverter/view/activities/FloatingPointConverterActivity.java
@@ -0,0 +1,16 @@
+package com.campos.baseconverter.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.campos.baseconverter.R;
+
+public class FloatingPointConverterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floating_point_converter);
+    }
+}
