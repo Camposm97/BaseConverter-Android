8d8bd481233e6d3fed090bc25e65ef3fe47b5544
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-17 23:50:16 -0400

    Create AppActivity.java

---
+++ app/src/main/java/com/campos/baseconverter/view/activity/AppActivity.java
@@ -0,0 +1,16 @@
+package com.campos.baseconverter.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.campos.baseconverter.R;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+    }
+}
