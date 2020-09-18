138ceac719fabd47b689e7201c190b9941c56b40
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-09-17 23:51:15 -0400

    Update AppActivity.java
    Added comment

--- app/src/main/java/com/campos/baseconverter/view/activity/AppActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activity/AppActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 import com.campos.baseconverter.R;
 
+/**
+ * I named it to AppActivity instead of MainActivity so the file can be listed at the top
+ * when you open the package folder.
+ */
 public class AppActivity extends AppCompatActivity {
 
     @Override
