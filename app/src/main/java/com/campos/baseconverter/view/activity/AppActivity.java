f6bac47a09a8c6ad9a4431c9f57e76048c103ee3
Camposm <camposm1205@gmail.com>
2020-09-19 15:25:26 -0400

    Update AppActivity.java

--- app/src/main/java/com/campos/baseconverter/view/activity/AppActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activity/AppActivity.java
@@ -2,16 +2,19 @@ package com.campos.baseconverter.view.activity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.campos.baseconverter.R;
+import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * I named it to AppActivity instead of MainActivity so the file can be listed at the top
- * when you open the package folder.
+ * when you open this package folder.
  */
-public class AppActivity extends AppCompatActivity {
+public class AppActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +26,11 @@ public class AppActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        return false;
     }
 }
