35a442d4df9d96dce341fd47a2460d4a9824669b
Camposm <camposm1205@gmail.com>
2020-09-19 16:08:15 -0400

    Update AppActivity.java
    Declaring attributes

--- app/src/main/java/com/campos/baseconverter/view/activity/AppActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activity/AppActivity.java
@@ -9,12 +9,19 @@ import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
 /**
  * I named it to AppActivity instead of MainActivity so the file can be listed at the top
  * when you open this package folder.
  */
 public class AppActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private FragmentManager fManager;
+    private NavigationView nView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +29,8 @@ public class AppActivity extends AppCompatActivity implements NavigationView.OnN
         setContentView(R.layout.activity_app);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
+    public void initControl() {
+        
     }
 
     @Override
