ba57b9336a767d4ece1b2feeac9a9171bfef74dc
Camposm <camposm1205@gmail.com>
2020-09-19 18:05:01 -0400

    Update AppActivity.java

--- app/src/main/java/com/campos/baseconverter/view/activity/AppActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activity/AppActivity.java
@@ -1,13 +1,7 @@
 package com.campos.baseconverter.view.activity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import com.campos.baseconverter.R;
-import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -16,6 +10,11 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
+
+import com.campos.baseconverter.R;
+import com.campos.baseconverter.view.fragment.AllBasesFragment;
+import com.campos.baseconverter.view.fragment.MainBasesFragment;
+import com.google.android.material.navigation.NavigationView;
 
 /**
  * I named it to AppActivity instead of MainActivity so the file can be listed at the top
@@ -43,21 +42,29 @@ public class AppActivity extends AppCompatActivity implements NavigationView.OnN
         navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                0, 0);
+                R.string.bt_yes, R.string.bt_no);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
     }
 
     public void initDisplay(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
-//            manager.beginTransaction().replace(R.id.fragment_container, new FragmentA()).commit();
-//            navigationView.setCheckedItem(R.id.fragment_a);
+            manager.beginTransaction().replace(R.id.fragment_container, new MainBasesFragment()).commit();
+            navigationView.setCheckedItem(R.id.item_main_bases);
         }
     }
 
     @Override // Do something if an item is clicked in drawer
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            
+            case R.id.item_main_bases:
+                manager.beginTransaction().replace(
+                        R.id.fragment_container, new MainBasesFragment()).commit();
+                break;
+            case R.id.item_all_bases:
+                manager.beginTransaction().replace(
+                        R.id.fragment_container, new AllBasesFragment()).commit();
+                break;
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
