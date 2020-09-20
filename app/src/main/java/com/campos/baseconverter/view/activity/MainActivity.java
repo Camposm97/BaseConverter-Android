e683f74574733f3d5b0b6c56135acefc9173a8ec
Camposm <camposm1205@gmail.com>
2020-09-20 15:30:18 -0400

    Renamed activity_app.xml -> activity_main.xml

--- app/src/main/java/com/campos/baseconverter/view/activity/MainActivity.java
+++ app/src/main/java/com/campos/baseconverter/view/activity/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app);
+        setContentView(R.layout.activity_main);
         initControls();
         initDisplay(savedInstanceState);
     }
