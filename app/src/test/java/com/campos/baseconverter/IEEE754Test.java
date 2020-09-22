776013e253f90b9d755e266886ed41c22aea13db
Camposm <camposm1205@gmail.com>
2020-09-22 13:05:03 -0400

    Update IEEE754Test.java

--- app/src/test/java/com/campos/baseconverter/IEEE754Test.java
+++ app/src/test/java/com/campos/baseconverter/IEEE754Test.java
@@ -10,7 +10,7 @@ public class IEEE754Test {
     @Test
     public void method() {
         IEEE754 ieee754 = new IEEE754();
-        BaseNumber input = new BaseNumber(Base.BASE_2, "1110.101");
+        BaseNumber input = new BaseNumber(Base.BASE_2, "100.01");
         ieee754.toSinglePrecision(input);
     }
 }
