3a7925c1a15a11b633cba1137527922d0e5cbe5a
Camposm <camposm1205@gmail.com>
2020-09-21 22:32:33 -0400

    Update IEEE754.java

--- app/src/main/java/com/campos/baseconverter/model/IEEE754.java
+++ app/src/main/java/com/campos/baseconverter/model/IEEE754.java
@@ -6,7 +6,7 @@ public class IEEE754 {
         String result = "";
         if (input.getBase().equals(Base.BASE_2)) {
             String value = input.getValue();
-
+            formatBinStr(value);
         }
         return result;
     }
@@ -15,9 +15,20 @@ public class IEEE754 {
      * result[0] contains value, result[1] contains power
      * @return result[]
      */
-    private String[] formatString(String value) {
+    private String[] formatBinStr(String value) {
         StringBuilder sb = new StringBuilder(value);
-
+        int j = 0;
+        for (int i = 0; i < sb.length(); i++) {
+            char c = sb.charAt(i);
+            if (c == '.') {
+                sb.deleteCharAt(i);
+                if (sb.charAt(0) == '1') {
+                    j = i - 1;
+                }
+            }
+        }
+        System.out.println(sb);
+        System.out.println("j=" + j);
         return null;
     }
 
