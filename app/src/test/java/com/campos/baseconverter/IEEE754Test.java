e543980ebb15064932cc73dd8748473e42dd864c
Camposm <camposm1205@gmail.com>
2020-09-21 22:24:37 -0400

    Create IEEE754Test.java

---
+++ app/src/test/java/com/campos/baseconverter/IEEE754Test.java
@@ -0,0 +1,16 @@
+package com.campos.baseconverter;
+
+import com.campos.baseconverter.model.Base;
+import com.campos.baseconverter.model.BaseNumber;
+import com.campos.baseconverter.model.IEEE754;
+
+import org.junit.Test;
+
+public class IEEE754Test {
+    @Test
+    public void method() {
+        IEEE754 ieee754 = new IEEE754();
+        BaseNumber input = new BaseNumber(Base.BASE_2, "1111.001");
+        ieee754.toSinglePrecision(input);
+    }
+}
