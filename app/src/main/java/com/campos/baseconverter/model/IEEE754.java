dd4ae7d9b9b3c22ae5ef6451a7f59234a2f89420
Camposm <camposm1205@gmail.com>
2020-09-21 22:24:39 -0400

    Update IEEE754.java

--- app/src/main/java/com/campos/baseconverter/model/IEEE754.java
+++ app/src/main/java/com/campos/baseconverter/model/IEEE754.java
@@ -2,10 +2,23 @@ package com.campos.baseconverter.model;
 
 public class IEEE754 {
 
-    public void toSinglePrecision(BaseNumber input) {
+    public String toSinglePrecision(BaseNumber input) {
+        String result = "";
         if (input.getBase().equals(Base.BASE_2)) {
             String value = input.getValue();
+
         }
+        return result;
+    }
+
+    /**
+     * result[0] contains value, result[1] contains power
+     * @return result[]
+     */
+    private String[] formatString(String value) {
+        StringBuilder sb = new StringBuilder(value);
+
+        return null;
     }
 
     private boolean containsFractions(String value) {
