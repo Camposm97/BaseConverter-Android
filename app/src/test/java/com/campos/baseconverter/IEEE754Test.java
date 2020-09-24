77c6dd344f8b9f77c53f3884447e986d349810f1
Camposm <camposm1205@gmail.com>
2020-09-23 22:52:25 -0400

    Update IEEE754Test.java
    Writing TODO

--- app/src/test/java/com/campos/baseconverter/IEEE754Test.java
+++ app/src/test/java/com/campos/baseconverter/IEEE754Test.java
@@ -7,9 +7,13 @@ import org.junit.Test;
 import java.util.Arrays;
 
 public class IEEE754Test {
+    /*
+    TODO:
+     - If you pass a value of 0.0 and pass that into formatBinStr, it will crash the program (StringIndexOutOfBounds)
+     */
     @Test
     public void testFormatBinStr1() {
-        String value = "00.00";
+        String value = "100.0";
         IEEE754 ieee = new IEEE754();
         String[] resultArr = ieee.formatBinStr(value);
         System.out.println(Arrays.toString(resultArr));
