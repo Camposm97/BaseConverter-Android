595a48df342408f16db276ca2617a74ee2f36166
Camposm <camposm1205@gmail.com>
2020-09-23 22:48:15 -0400

    Update D.java
    Renamed Tag to 'D', D for debug

---
+++ app/src/main/java/com/campos/baseconverter/util/D.java
@@ -0,0 +1,5 @@
+package com.campos.baseconverter.util;
+
+public class D {
+    public static final String E = "Campos";
+}
--- app/src/main/java/com/campos/baseconverter/util/Tag.java
+++
@@ -1,5 +0,0 @@
-package com.campos.baseconverter.util;
-
-public class Tag {
-    public static final String TAG = "Campos";
-}
