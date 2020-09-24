65bba60f00cb720a6ec1153ad2fd40b197b2013a
Camposm <camposm1205@gmail.com>
2020-09-23 22:48:49 -0400

    Update IEEE754Test.java
    Updating Tag class references

--- app/src/test/java/com/campos/baseconverter/IEEE754Test.java
+++ app/src/test/java/com/campos/baseconverter/IEEE754Test.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 public class IEEE754Test {
     @Test
     public void testFormatBinStr1() {
-        String value = "00.11";
+        String value = "00.00";
         IEEE754 ieee = new IEEE754();
         String[] resultArr = ieee.formatBinStr(value);
         System.out.println(Arrays.toString(resultArr));
