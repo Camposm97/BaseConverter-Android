9c1050a812883b3838a16d57910a32a1bb478371
Camposm <camposm1205@gmail.com>
2020-09-25 17:00:33 -0400

    Update AlertUtils.java
    Deleted showInvalidBaseNumInput()

--- app/src/main/java/com/campos/baseconverter/util/AlertUtils.java
+++ app/src/main/java/com/campos/baseconverter/util/AlertUtils.java
@@ -9,8 +9,4 @@ public class AlertUtils {
     public static void showShortToast(Context c, String text) {
         Toast.makeText(c, text, Toast.LENGTH_SHORT).show();
     }
-
-    public static void showInvalidBaseNumInput(Context c) {
-        Toast.makeText(c, R.string.invalid_base_num_message, Toast.LENGTH_SHORT).show();
-    }
 }
