4ede6e9d08e0cf999835077459123bf980714091
Camposm <camposm1205@gmail.com>
2020-09-25 16:50:21 -0400

    Update BinaryOperator.java
    Implementing instance methods for flipping bits and adding one bit

--- app/src/main/java/com/campos/baseconverter/model/BinaryOperator.java
+++ app/src/main/java/com/campos/baseconverter/model/BinaryOperator.java
@@ -3,7 +3,29 @@ package com.campos.baseconverter.model;
 public class BinaryOperator {
     public String toTwosComp(BaseNumber input) {
         StringBuilder sb = new StringBuilder();
-    
+        if (input.isValidAndIs(Base.BASE_2)) {
+
+        }
         return sb.toString();
+    }
+
+    public char[] flipBits(char[] input) {
+        char[] arr = input.clone();
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+            if (c == 48)
+                arr[i] = 49;
+            if (c == 49)
+                arr[i] = 48;
+        }
+        return arr;
+    }
+
+    public char[] addOne(char[] input) {
+        char[] arr = input.clone();
+        for (int i = 0; i < arr.length; i++) {
+            
+        }
+        return arr;
     }
 }
