c95ea71604e79967aa88f9f965b1373067d9ce15
Camposm <camposm1205@gmail.com>
2020-09-25 17:38:14 -0400

    Update BinaryOperatorTest.java
    Tested flipBits() (successful)

--- app/src/test/java/com/campos/baseconverter/BinaryOperatorTest.java
+++ app/src/test/java/com/campos/baseconverter/BinaryOperatorTest.java
@@ -1,10 +1,18 @@
 package com.campos.baseconverter;
 
+import com.campos.baseconverter.model.BinaryOperator;
+
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class BinaryOperatorTest {
     @Test
     public void testFlipBits() {
-        
+        String s = "1001";
+        BinaryOperator op = new BinaryOperator();
+        char[] result = op.flipBits(s.toCharArray());
+        char[] expected = {'0', '1', '1', '0'};
+        assertArrayEquals(expected, result);
     }
 }
