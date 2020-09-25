6c0838ad9c5c0e35f38d254c87ed5bc1066b00a3
Camposm <camposm1205@gmail.com>
2020-09-25 16:58:17 -0400

    Update AlertUtils.java
    Implemented class method for making a short toast

--- app/src/main/java/com/campos/baseconverter/util/AlertUtils.java
+++ app/src/main/java/com/campos/baseconverter/util/AlertUtils.java
@@ -6,6 +6,10 @@ import android.widget.Toast;
 import com.campos.baseconverter.R;
 
 public class AlertUtils {
+    public static void showShortToast(Context c, String text) {
+        Toast.makeText(c, text, Toast.LENGTH_SHORT).show();
+    }
+
     public static void showInvalidBaseNumInput(Context c) {
         Toast.makeText(c, R.string.invalid_base_num_message, Toast.LENGTH_SHORT).show();
     }
