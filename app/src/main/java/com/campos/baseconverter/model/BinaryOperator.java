655bae3a0525ce923f0a6680de57861f345bbc5a
Camposm <camposm1205@gmail.com>
2020-09-25 16:26:44 -0400

    Update BinaryOperator.java
    Writing a method for converting a binary number to two's complement

--- app/src/main/java/com/campos/baseconverter/model/BinaryOperator.java
+++ app/src/main/java/com/campos/baseconverter/model/BinaryOperator.java
@@ -1,5 +1,9 @@
 package com.campos.baseconverter.model;
 
 public class BinaryOperator {
+    public String toTwosComp(BaseNumber input) {
+        StringBuilder sb = new StringBuilder();
     
+        return sb.toString();
+    }
 }
