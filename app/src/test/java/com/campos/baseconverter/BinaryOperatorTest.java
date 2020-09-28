105fbefd29b977eb5373d6dfd5d0a600b45d0bdd
Camposm <camposm1205@gmail.com>
2020-09-27 20:48:43 -0400

    Update BinaryOperatorTest.java
    Wrote a test for adding one (1111)

--- app/src/test/java/com/campos/baseconverter/BinaryOperatorTest.java
+++ app/src/test/java/com/campos/baseconverter/BinaryOperatorTest.java
@@ -17,11 +17,20 @@ public class BinaryOperatorTest {
     }
 
     @Test
-    public void testAddOne() {
+    public void testAddOne1() {
         String s = "1001";
         BinaryOperator op = new BinaryOperator();
         char[] result = op.addOne(s.toCharArray());
         char[] expected = "1010".toCharArray();
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testAddOne2() {
+        String s = "1111";
+        BinaryOperator op = new BinaryOperator();
+        char[] result = op.addOne(s.toCharArray());
+        char[] expected = "0000".toCharArray();
         assertArrayEquals(expected, result);
     }
 }
